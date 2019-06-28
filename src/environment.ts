8f1617659deaadd30ee61b7f29e7a27c909d729f
= <odus@knights.ucf.edu>
2019-06-28 07:54:53 -0400

    Add Dockerfile

--- src/environment.ts
+++ src/environment.ts
@@ -5,6 +5,7 @@ config({ path: resolve(__dirname, '../.env') });
 
 const Environment = {
   DiscordToken: process.env.DISCORD_TOKEN,
+  Playground: process.env.NODE_ENV
 };
 
 export default Environment;
