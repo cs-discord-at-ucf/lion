f0e2f8de947eb7f1651f8df7b2e38b876d285447
c650 <c650@users.noreply.github.com>
2019-08-19 11:06:53 -0400

    Adds capability to filter listclasses with cs/it arg
    Presents full list if argument is invalid

--- src/app/plugins/listclasses.plugin.ts
+++ src/app/plugins/listclasses.plugin.ts
@@ -16,28 +16,45 @@ export class ListClassesPlugin extends Plugin {
     const itClasses = this.container.classService.getClasses(ClassType.IT);
     let response = '```\n';
 
-    response += 'CS Classes:\n';
+    let listCsClasses = !args || args.length == 0 || args[0].toLowerCase() == 'cs';
+    let listItClasses = !args || args.length == 0 || args[0].toLowerCase() == 'it';
 
-    const csClassNames: string[] = [];
-    for (const classObj of csClasses) {
-      csClassNames.push(classObj[1].name);
+    const listingSomething = listCsClasses || listItClasses;
+
+    // if we aren't listing anything due to bad args, we list everything.
+    listCsClasses = listCsClasses || !listingSomething;
+    listItClasses = listItClasses || !listingSomething;
+
+    if (listCsClasses) {
+      response += 'CS Classes:\n';
+
+      const csClassNames: string[] = [];
+      for (const classObj of csClasses) {
+        csClassNames.push(classObj[1].name);
+      }
+
+      csClassNames.sort().forEach((classObjname) => {
+        response += `${classObjname}\n`;
+      });
     }
 
-    csClassNames.sort().forEach((classObjname) => {
-      response += `${classObjname}\n`;
-    });
+    if (listItClasses) {
+      const itClassNames: string[] = [];
+      for (const classObj of itClasses) {
+        itClassNames.push(classObj[1].name);
+      }
 
-    const itClassNames: string[] = [];
-    for (const classObj of itClasses) {
-      itClassNames.push(classObj[1].name);
+      response += `${listCsClasses ? '\n' : ''}IT Classes:\n`;
+      itClassNames.sort().forEach((classObjname) => {
+        response += `${classObjname}\n`;
+      });
     }
-
-    response += '\nIT Classes:\n';
-    itClassNames.sort().forEach((classObjname) => {
-      response += `${classObjname}\n`;
-    });
 
     response += '\n```\n You can register for classes through the `!register` command.';
+
+    if (!listingSomething)
+      response += '\n**The filter supplied is invalid; everything is listed above.**';
+
     message.reply(response);
   }
 }
