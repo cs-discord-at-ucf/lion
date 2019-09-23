9f86df9c882313594dca5be9e3c042f9168d1d83
c650 <c650@users.noreply.github.com>
2019-09-23 18:14:47 -0400

    Add require url handler (#54)

---
+++ src/app/handlers/require_url.handler.ts
@@ -0,0 +1,22 @@
+import { IHandler, IMessage, IContainer } from '../../common/types';
+import Constants from '../../common/constants';
+import { TextChannel } from 'discord.js';
+
+export class RequireUrlHandler implements IHandler {
+  private _url_regex: RegExp = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+  private _channels: String[] = [ Constants.Channels.Public.HelpfulBaubles ];
+
+  constructor(public container: IContainer) {}
+
+  public async execute(message: IMessage) {
+    const channelObj: TextChannel = this.container.clientService.channels.get(message.channel.id) as TextChannel;
+    if (!this._channels.includes(channelObj.name)) {
+      return;
+    }
+
+    if (!message.content.toLowerCase().match(this._url_regex)) {
+      message.author.send(`Hey ${message.author},\n We require for you to include a link to your message in the #${channelObj.name} channel.\n\n Here's your message content:\`\`\`${message.content}\`\`\` `);
+      message.delete();
+    }
+  }
+}
--- src/services/handler.service.ts
+++ src/services/handler.service.ts
@@ -1,6 +1,7 @@
 import { BlacklistHandler } from '../app/handlers/blacklist.handler';
+import { RequireUrlHandler } from '../app/handlers/require_url.handler';
 import { CommandHandler } from '../app/handlers/command.handler';
 
 export class HandlerService {
-  public handlers = [BlacklistHandler, CommandHandler];
+  public handlers = [BlacklistHandler, CommandHandler, RequireUrlHandler];
 }
