3a74917c3a361017ff188f410a594221617aa52b
gooca <nicholasjalemao@gmail.com>
2019-10-18 11:05:42 -0400

    Create 8ball plugin (#63)

--- src/bootstrap/plugin.loader.ts
+++ src/bootstrap/plugin.loader.ts
@@ -1,6 +1,7 @@
 import { DogPlugin } from '../app/plugins/dog.plugin';
 import { UserCountPlugin } from '../app/plugins/users.plugin';
 import { GaragePlugin } from '../app/plugins/garage.plugin';
+import { EightBallPlugin } from '../app/plugins/8ball.plugin';
 import { WeatherPlugin } from '../app/plugins/weather.plugin';
 import { HelpPlugin } from '../app/plugins/help.plugin';
 import { RegisterPlugin } from '../app/plugins/register.plugin';
@@ -11,17 +12,18 @@ import { DelRolesPlugin } from '../app/plugins/delroles.plugin';
 import { ListRolesPlugin } from '../app/plugins/listroles.plugin';
 
 const PluginStore: { [pluginName: string]: any } = {
-  dog: DogPlugin,
-  users: UserCountPlugin,
-  garage: GaragePlugin,
-  weather: WeatherPlugin,
-  help: HelpPlugin,
-  register: RegisterPlugin,
-  unregister: UnregisterPlugin,
-  listclasses: ListClassesPlugin,
-  addroles: AddRolesPlugin,
-  delroles: DelRolesPlugin,
-  listroles: ListRolesPlugin,
+  "dog": DogPlugin,
+  "users": UserCountPlugin,
+  "garage": GaragePlugin,
+  "8ball": EightBallPlugin,
+  "weather": WeatherPlugin,
+  "help": HelpPlugin,
+  "register": RegisterPlugin,
+  "unregister": UnregisterPlugin,
+  "listclasses": ListClassesPlugin,
+  "addroles": AddRolesPlugin,
+  "delroles": DelRolesPlugin,
+  "listroles": ListRolesPlugin,
 };
 
 export class PluginLoader {
