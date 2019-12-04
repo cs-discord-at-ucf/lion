a915b3ecfcf2ac535cd85a45f2a1d51f663171a0
c650 <c650@users.noreply.github.com>
2019-12-04 11:14:13 -0500

    [stocks improvement] handle multiple stocks in one command, refactor color stuff

--- src/app/plugins/stock.plugin.ts
+++ src/app/plugins/stock.plugin.ts
@@ -12,6 +12,20 @@ export class StockPlugin extends Plugin {
   private _API_URL: string = 'https://www.alphavantage.co/query?';
   private _ADVANCED_QUOTE_LINK: string = 'https://finance.yahoo.com/quote/';
 
+  private _STONK_IMG = {
+    up: {
+      thumbnail_url:
+        'https://www.netclipart.com/pp/m/59-594517_arrow-going-up-png-stock-market-graph-up.png',
+      color: '#00ff00',
+      direction: '+',
+    },
+    down: {
+      thumbnail_url: 'https://claytrader.com/wp-content/uploads/2014/12/IMG_26122014_144211.png',
+      color: '#ff0000',
+      direction: '', // no direction needed because floatToString will add `-`
+    },
+  };
+
   constructor(public container: IContainer) {
     super();
   }
@@ -22,8 +36,25 @@ export class StockPlugin extends Plugin {
   }
 
   public async execute(message: IMessage, args: string[]) {
-    const ticker = args[0];
+    const bad_queries: string[] = [];
 
+    for (const ticker of args) {
+      const quote = await this._queryStock(ticker);
+
+      if (!quote) {
+        bad_queries.push(ticker);
+        continue;
+      }
+
+      message.channel.send(this._makeEmbed(quote));
+    }
+
+    if (bad_queries.length > 0) {
+      message.reply(`Erroneous queries: \`${bad_queries.join(', ')}\``);
+    }
+  }
+
+  private async _queryStock(ticker: string) {
     const call_url =
       this._API_URL +
       'function=GLOBAL_QUOTE' +
@@ -43,46 +74,31 @@ export class StockPlugin extends Plugin {
 
     const quote = data['Global Quote'];
 
-    if (!quote) {
-      message.reply('Query of `' + args.join(' ') + '` resulted in an error.');
-      return;
-    }
+    return quote;
+  }
 
+  private _makeEmbed(quote: any) {
     const embed: RichEmbed = new RichEmbed();
 
     let direction = '';
 
-    if (parseFloat(quote['09. change']) < 0) {
-      // if stock is down
-      embed.setColor('#ff0000');
-      embed.setThumbnail(
-        'https://claytrader.com/wp-content/uploads/2014/12/IMG_26122014_144211.png'
-      );
-    } else {
-      direction = '+';
-      // if stock is up
-      embed.setColor('#00ff00');
-      embed.setThumbnail(
-        'https://www.netclipart.com/pp/m/59-594517_arrow-going-up-png-stock-market-graph-up.png'
-      );
-    }
+    const colorThumbnailDirection =
+      parseFloat(quote['09. change']) < 0 ? this._STONK_IMG.down : this._STONK_IMG.up;
+
+    direction = colorThumbnailDirection.direction;
+    embed.setColor(colorThumbnailDirection.color);
+    embed.setThumbnail(colorThumbnailDirection.thumbnail_url);
 
     embed.setTitle(quote['01. symbol'] + ' @ ' + quote['05. price']);
     embed.setFooter('data from https://www.alphavantage.co/');
 
     embed.addField('Change', direction + this._formatNum(quote['10. change percent']) + '%', false);
 
-    if (args.length > 1 && args[1] === '-ohl') {
-      embed.addField('Open', this._formatNum(quote['02. open']), true);
-      embed.addField('Low', this._formatNum(quote['04. low']), true);
-      embed.addField('High', this._formatNum(quote['03. high']), true);
-    }
-
     embed.setTimestamp(new Date());
 
     embed.setURL(this._ADVANCED_QUOTE_LINK + quote['01. symbol']);
 
-    message.channel.send(embed);
+    return embed;
   }
 
   private _formatNum(num: string) {
