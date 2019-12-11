f9cd8a94c5daac79dd5687651de72d41e02aa999
c650 <c650@users.noreply.github.com>
2019-12-11 16:42:08 -0500

    changes for cr

--- src/app/plugins/delclasschans.plugin.ts
+++ src/app/plugins/delclasschans.plugin.ts
@@ -2,7 +2,7 @@ import { Plugin } from '../../common/plugin';
 import { IContainer, IMessage, ChannelType, ClassType } from '../../common/types';
 
 export class DeleteClassChannelsPlugin extends Plugin {
-  public name: string = ' Delete classes';
+  public name: string = 'Delete classes';
   public description: string = "Nukes every class channel. Don't be idiot";
   public usage: string = 'delclasschans [super secret password]';
   public permission: ChannelType = ChannelType.Admin;
@@ -11,23 +11,14 @@ export class DeleteClassChannelsPlugin extends Plugin {
     super();
   }
 
-  public validate(message: IMessage, args: string[]) {
-    return (
-      args.join(' ') ===
-      'Yes I understand that this is a really really serious thing to do and I will not be stupid and do it prematurely or maliciously.'
-    );
-  }
-
   public async execute(message: IMessage, args: string[]) {
     const channels = this.container.classService.getClasses(ClassType.ALL);
     const numChannels = channels.size;
-
     const deleteCaller = message.author.tag;
 
-    await message.reply(`Deleting ${numChannels} channels at request of ${deleteCaller}`);
+    message.reply(`Deleting **${numChannels}** channels at request of **${deleteCaller}**`);
 
     channels.forEach((channel, _) => {
-      console.log(`Channel ${channel.name} deleted by ${deleteCaller}`);
       channel.delete();
     });
   }
