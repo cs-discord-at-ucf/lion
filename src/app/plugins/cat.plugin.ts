f0c76162429b2949870f47c40815377750afebc5
CallumCoy <callumcoy@knights.ucf.edu>
2020-05-08 00:03:26 -0400

    removal of clutter, along with some cleaning
    Plugin no longer supports the bomb command.
    should be clean enough to pass the linter test.
    made the cat breed/ID a object, to help keep the items together.

--- src/app/plugins/cat.plugin.ts
+++ src/app/plugins/cat.plugin.ts
@@ -2,6 +2,11 @@ import Constants from '../../common/constants';
 import { Plugin } from '../../common/plugin';
 import { ChannelType, IContainer, IHttpResponse, IMessage } from '../../common/types';
 
+class Breed {
+    public breed: string = '';
+    public id: string = '';
+  }
+
 export class CatPlugin extends Plugin {
   public name: string = 'Cat Plugin';
   public description: string = 'Generates pictures of cats, via popular command.';
@@ -10,8 +15,8 @@ export class CatPlugin extends Plugin {
   public pluginChannelName: string = Constants.Channels.Public.Pets;
 
   private _API_URL: string = 'https://api.thecatapi.com/v1/';
-  private _breeds: string[] = [];
-  private _breedsID: string[] = [];
+  private _breadCat: string = `https://www.thetoychronicle.com/wp-content/uploads/2016/05/Box-cat-Bread-cat-Rato-workroom-Kim-.jpg`;
+  private _cats: Breed[] = [];
 
   constructor(public container: IContainer) {
     super();
@@ -21,118 +26,75 @@ export class CatPlugin extends Plugin {
         const breeds = response.data;
 
         for (let index = 0; index < breeds.length; index++) {
-            const breed = breeds[index].name;
-            const breedID = breeds[index].id;
-            this._breeds.push(breed.toLowerCase())
-            this._breedsID.push(breedID.toLowerCase())
+
+            const breedInfo = {
+                breed: breeds[index].name.toLowerCase(),
+                id: breeds[index].id.toLowerCase(),
+            };
+
+            this._cats.push(breedInfo);
         }
-    
-        // Doesn't work due to the the way the JSON info is now listed, Would be interested in how to do it with foreach if anyone could tell me.
-        // breeds.forEach((breed) => {
-        //   this._breeds.push(breed.toLowerCase());
-        // });
       })
       .catch((err) => console.log(err));
   }
 
-  public async execute(message: IMessage, args?: string[]){
-
-    var command = this._parseCommand(args || []);
+  public async execute(message: IMessage, args?: string[]) {
     
-    if (command.includes("fact")) {
-
-        // fact is not yet supported by the API at this time however this code should work upon its release
-        
-        // command = command.replace("fact", "");
-        // if (command.includes("breeds")) {
-
-        //     message.reply(`Breeds supported: \n\`\`\`\n${this._breeds.join('\n')}\`\`\``);
-        //     return;
-
-        // } else {
-
-        //     var search = "";
-
-        //     if (command != "fact") {
-
-        //         search = command.replace("fact", "");
-
-
-        //         const index = this._breeds.findIndex((curBreed) => {
-        //             return command.includes(curBreed);
-        //         })
-
-        //         breed = `breed_id=${this._breedsID[index]}`;
-        //     }
-
-        //     await this.container.httpService
-        //     .get(`${this._API_URL}facts/${search}`)
-        //     .then((response: IHttpResponse) => {
-
-        //         message.reply('', {
-        //             files: [response.data[0].text],
-        //         });
-        //     })
-
-        //     .catch((err) => console.log(err));
-
-        // }
-
-    } else if (command == "breeds" || command == "breed") {
+    let command = this._parseCommand(args || []);
+    
+    if (command === "breeds" || command === "breed") {
         
         //Simply return the list of suported breeds
-        message.reply(`Breeds supported: \n\`\`\`\n${this._breeds.join('\n')}\`\`\``);
+        let reply = "";
+
+        this._cats.forEach(cat => {
+            reply = reply.concat(cat.breed, "\n");
+        });
+
+        message.reply(`Breeds supported: \n\`\`\`\n${reply}\`\`\``);
         return;
 
-    } else if (command == "breads" || command == "bread") {
+    } else if (command === "breads" || command === "bread") {
 
         // the Easter Egg, my gawd
         message.reply('', {
-            files: [`https://www.thetoychronicle.com/wp-content/uploads/2016/05/Box-cat-Bread-cat-Rato-workroom-Kim-.jpg`],
+            files: [this._breadCat],
         });
 
-    } else if (command.includes("pic")){
+    } else if (command.includes("pic")) {
         // lets the system know that pic has been determined
 
         command = command.replace("pic", "");
-        var breed = "";
-        var count = 1;
-
-        if (command.includes("bomb")){
-            //processes that the user wants 3 pictures
-            count = 3;
-            command = command.replace("bomb", "");
-        }
+        let breed: string = "";
 
         // determines that their was aditional text added on
         if (command.trim() != "") {
 
             //finds the breed
-            const index = this._breeds.findIndex((curBreed) => {
-                return command.includes(curBreed);
-            })
-
-            console.log(index)
+            this._cats.forEach(cat => {
+                if (command.includes(cat.breed)) {
+                    breed = cat.id;
+                    return;
+                }
+            });
 
             // checks if the bread was found or not
-            if (index == -1) {
-                message.reply(`I could not find that breed, take this instead.  Please type, "cat breed" to get a list of compatible breeds.`);
+            if (breed === "") {
+                message.reply(`I could not find that breed. Have this picture instead. Please type, "cat breed" to get a list of compatible breeds.`);
             } else {
-                breed = `&breed_id=${this._breedsID[index]}`;
+                breed = `&breed_id=${breed}`;
             }
         }
         
         //recieves the according info and posts, or derps
         await this.container.httpService
-        .get(`${this._API_URL}images/search?limit=${count}${breed}`)
+        .get(`${this._API_URL}images/search?limit=1${breed}`)
         .then((response: IHttpResponse) => {
-            for (let index = 0; index < count; index++) {
-                message.reply('', {
-                    files: [response.data[index].url],
-                });
-            }
+            message.reply('', {
+                files: [response.data[0].url],
+            });
         })
-        .catch((err) => console.log(err));
+        .catch(console.log);
     }
   }
 
