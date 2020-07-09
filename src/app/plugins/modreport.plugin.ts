b98ea7a28a72e936ddf9ea7345811dd2c8454547
c650 <c650@users.noreply.github.com>
2020-07-09 19:41:37 -0400

    Add warning and banning to modreport

--- src/app/plugins/modreport.plugin.ts
+++ src/app/plugins/modreport.plugin.ts
@@ -1,7 +1,8 @@
 import Constants from '../../common/constants';
 import { Plugin } from '../../common/plugin';
 import { ChannelType, IContainer, IMessage } from '../../common/types';
-import { Report } from '../../services/report.service';
+import { Report, ReportService } from '../../services/report.service';
+import { Snowflake } from 'discord.js';
 
 export class ModReportPlugin extends Plugin {
   public name: string = 'Mod Report Plugin';
@@ -10,7 +11,7 @@ export class ModReportPlugin extends Plugin {
   public permission: ChannelType = ChannelType.Staff;
   public pluginChannelName: string = Constants.Channels.Staff.UserOffenses;
 
-  private _commandPattern: RegExp = /(add|list)\s+([^#]+#\d{4})\s*(.*)/;
+  private _commandPattern: RegExp = /(add|list|warn|ban)\s+([^#]+#\d{4})\s*(.*)/;
 
   constructor(public container: IContainer) {
     super();
@@ -33,6 +34,10 @@ export class ModReportPlugin extends Plugin {
         await this._handleAddReport(message, user_handle, description);
       } else if (sub_command === 'list') {
         await this._handleListReport(message, user_handle);
+      } else if (sub_command === 'warn') {
+        await this._handleIssueWarning(message, user_handle, description);
+      } else if (sub_command === 'ban') {
+        await this._handleIssueBan(message, user_handle, description);
       } else {
         message.reply('Invalid command. See !help');
       }
@@ -42,7 +47,7 @@ export class ModReportPlugin extends Plugin {
     }
   }
 
-  private async _handleAddReport(message: IMessage, user_handle: string, description?: string) {
+  private _fileReport(message: IMessage, user_handle: string, description?: string) {
     const rep: Report = new Report(
       message.guild,
       user_handle,
@@ -52,6 +57,32 @@ export class ModReportPlugin extends Plugin {
 
     this.container.reportService.addReport(rep);
 
+    return rep;
+  }
+
+  private async _sendWarning(rep: Report) {
+    await this._sendModMessageToUser(
+      'Hey there, this is a courtesy warning that you violated the code of conduct and a report was filed with me.',
+      rep
+    );
+  }
+
+  private async _sendBan(rep: Report) {
+    await this.container.guildService.get().ban(rep.user, { reason: rep.description });
+
+    await this._sendModMessageToUser('Hey there. You have been banned', rep);
+  }
+
+  private async _sendModMessageToUser(message: string, rep: Report) {
+    await this.container.clientService.users
+      .get(rep.user)
+      ?.send(`${message} Reason: ${rep.description || 'nothing specific'}`, {
+        files: rep.attachments,
+      });
+  }
+
+  private async _handleAddReport(message: IMessage, user_handle: string, description?: string) {
+    const rep = this._fileReport(message, user_handle, description);
     message.reply(`Added report on ${user_handle}: ${rep.toString()}`);
   }
 
@@ -62,5 +93,25 @@ export class ModReportPlugin extends Plugin {
         ? `No reports on ${user_handle}`
         : `***Here are the reports on ${user_handle}***\n${msg}`
     );
+  }
+
+  private async _handleIssueWarning(message: IMessage, user_handle: string, description?: string) {
+    const rep = this._fileReport(message, user_handle, description);
+
+    await this._sendWarning(rep);
+
+    message.reply(`Added report and warned ${user_handle}: ${rep.toString()}`);
+  }
+
+  private async _handleIssueBan(message: IMessage, user_handle: string, description?: string) {
+    const rep = this._fileReport(
+      message,
+      user_handle,
+      `BANNED for ${description || 'no specific reason'}`
+    );
+
+    await this._sendBan(rep);
+
+    message.reply(`Banned ${user_handle}: ${rep.toString()}`);
   }
 }
