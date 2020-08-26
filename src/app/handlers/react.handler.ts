989585fb0ce78f3dc85f5a57037de8e83cd63a84
c650 <c650@users.noreply.github.com>
2020-08-26 12:59:10 -0400

    create react.handler.ts to let users vote to pin messages

---
+++ src/app/handlers/react.handler.ts
@@ -0,0 +1,34 @@
+import { TextChannel, MessageReaction } from 'discord.js';
+import Constants from '../../common/constants';
+import { IContainer, IHandler, IMessage, ClassType } from '../../common/types';
+
+export class ReactHandler implements IHandler {
+  private _PIN_THRESH = 5;
+
+  constructor(public container: IContainer) {}
+
+  public async execute(message: IMessage): Promise<void> {
+    const channel = message.channel as TextChannel;
+
+    this._handleClassChannelPinRequest(message, channel);
+  }
+  private async _handleClassChannelPinRequest(message: IMessage, channel: TextChannel) {
+    if (!this.container.classService.getClasses(ClassType.ALL).has(channel.name)) {
+      return;
+    }
+
+    if (message.pinned || !message.pinnable) {
+      return;
+    }
+
+    const onlyPin = (react: MessageReaction) => react.emoji.name === '📌';
+
+    message
+      .awaitReactions(onlyPin, { max: this._PIN_THRESH })
+      .then((collection) => {
+        const count = collection.first()?.count;
+        count >= this._PIN_THRESH && message.pin();
+      })
+      .catch();
+  }
+}
--- src/services/handler.service.ts
+++ src/services/handler.service.ts
@@ -3,6 +3,7 @@ import { ClassChannelHandler } from '../app/handlers/class_channel.handler';
 import { CommandHandler } from '../app/handlers/command.handler';
 import { RequireUrlHandler } from '../app/handlers/require_url.handler';
 import { TagRateLimitHandler } from '../app/handlers/tag_rate_limit.handler';
+import { ReactHandler } from '../app/handlers/react.handler';
 
 export class HandlerService {
   public messageHandlers = [
@@ -10,6 +11,7 @@ export class HandlerService {
     CommandHandler,
     RequireUrlHandler,
     TagRateLimitHandler,
+    ReactHandler,
   ];
 
   public channelHandlers = [ClassChannelHandler];
