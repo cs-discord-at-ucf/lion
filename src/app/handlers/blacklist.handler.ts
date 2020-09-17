9b7029aaee244f1a8142df8f04b8ff6992b342d0
serspencer <ser8@cfl.rr.com>
2020-09-17 16:24:47 -0400

    Merged Zoom; VaderSentiment; Word BlackList

--- src/app/handlers/blacklist.handler.ts
+++ src/app/handlers/blacklist.handler.ts
@@ -2,6 +2,11 @@ import { TextChannel } from 'discord.js';
 import Constants from '../../common/constants';
 import { IContainer, IHandler, IMessage, ClassType } from '../../common/types';
 import { Moderation } from '../../services/moderation.service';
+import { readFileSync } from 'fs';
+
+const vader = require('vader-sentiment');
+
+export const blockedWordsLine = (readFileSync('src/common/wordblacklist.txt', 'utf-8')).split(/\r?\n/);
 
 interface LinkLabel {
   regex: RegExp;
@@ -15,6 +20,7 @@ export class BlacklistHandler implements IHandler {
     { regex: /chegg\.com/, label: 'Chegg' },
     { regex: /coursehero\.com/, label: 'CourseHero' },
     { regex: /quizlet\.com/, label: 'Quizlet' },
+    { regex: /ucf\.zoom\.us/, label: 'Zoom' },
   ];
 
   private _whitelistedChannels = new Set([Constants.Channels.Public.Clubs]);
@@ -22,17 +28,37 @@ export class BlacklistHandler implements IHandler {
 
   public async execute(message: IMessage): Promise<void> {
     const channel = message.channel as TextChannel;
+    const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(message.content);
 
     if (this._whitelistedChannels.has(channel.name)) {
       return;
     }
 
+    //Word Black List
+    blockedWordsLine.forEach((blockedWords) => {
+      if (message.content.toLocaleLowerCase().includes(blockedWords)) {
+        this.container.messageService.sendBotReport(message, "Blocked Word");
+        message.delete();
+        return;
+      }
+      return;
+    });
+    
+    //VaderSentiment Negativity Check
+    console.log(intensity);
+    if (intensity['compound'] < 0 ) {
+      this.container.messageService.sendBotReport(message, "Negativity");
+      // message.delete();  //Optional Delete Negative Messages
+      return;
+    }
+
+    //Link Checking
     this._expressions.forEach(({ regex, label }) => {
       if (message.content.toLowerCase().match(regex)) {
         message.author.send(
           `Please do not share \`${label}\` links in the \`${message.guild.name}\` server.`
         );
-        this.container.messageService.sendBotReport(message);
+        this.container.messageService.sendBotReport(message, "Link Sharing");
         const rep = new Moderation.Report(
           message.guild,
           message.author.tag,
@@ -43,21 +69,14 @@ export class BlacklistHandler implements IHandler {
       }
     });
 
-    if (message.content.toLowerCase().match(/ucf.zoom.us/)) {
-      message.author.send(
-        'Hey, we are currently not allowing for UCF Zoom links to be posted within the Discord.'
-      );
-      this.container.messageService.sendBotReport(message);
-      message.delete();
-      return;
-    }
-
+    // Code and Screenshot sharing in Classes
     const isClassChannel = this.container.classService.getClasses(ClassType.ALL).has(channel.name);
     const hasBackticks = message.content.toLowerCase().match(/```/);
     const hasAttachment = message.attachments.size;
 
     if (isClassChannel && (hasBackticks || hasAttachment)) {
-      this.container.messageService.sendBotReport(message);
+      this.container.messageService.sendBotReport(message, "Code");
+      return;
     }
   }
 }
