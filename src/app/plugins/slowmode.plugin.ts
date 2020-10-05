3dd9c1047de77507659382723b93a7251a97490a
c650 <c650@users.noreply.github.com>
2020-10-05 10:05:19 -0400

    refactor

--- src/app/plugins/slowmode.plugin.ts
+++ src/app/plugins/slowmode.plugin.ts
@@ -1,4 +1,4 @@
-import { GuildChannel, TextChannel } from 'discord.js';
+import { TextChannel } from 'discord.js';
 import Constants from '../../common/constants';
 import { Plugin } from '../../common/plugin';
 import { ChannelType, IContainer, IMessage } from '../../common/types';
@@ -11,33 +11,15 @@ export class SlowModePlugin extends Plugin {
   public permission: ChannelType = ChannelType.Staff;
   public pluginChannelName: string = Constants.Channels.Staff.ModChat;
 
-  private _commandPattern: RegExp = /(\d+)\s+(\d+)\s+((?:<#(?:\d+)>\s*)+)/;
-  private _channelIDRegex: RegExp = /<#(\d+)>/g;
-
   constructor(public container: IContainer) {
     super();
   }
 
-  public validate(message: IMessage, args?: string[]) {
-    return !!args && this._commandPattern.test(args.join(' '));
+  public validate(_message: IMessage, args?: string[]) {
+    return !!args && args.length >= 3;
   }
 
   public async execute(message: IMessage, args: string[]) {
-    const match = args.join(' ').match(this._commandPattern);
-
-    // never happens, but make linter happy
-    if (!match) {
-      return;
-    }
-
-    const [_, expiration, slowmodeSetting, channels] = match;
-
-    const channel_objs =
-      channels
-        .match(this._channelIDRegex)
-        ?.map((c) => this.container.guildService.get().channels.get(c.replace(/\D/g, '')))
-        .filter((c) => c !== undefined) || [];
-
     const createUndoFunc = (channel: TextChannel) => {
       const f = async () => {
         this.container.loggerService.info(`turning off slowmode in ${channel.name}`);
@@ -46,18 +28,34 @@ export class SlowModePlugin extends Plugin {
       return f;
     };
 
+    // always at least three arguments per validate(...)
+    const [expiration, slowmodeSetting, ...channels] = args;
+
     const expDate = new Date();
     expDate.setSeconds(expDate.getSeconds() + +expiration);
 
-    for (const channel of channel_objs as GuildChannel[]) {
-      const c = channel as TextChannel;
+    channels
+      .reduce((acc: TextChannel[], cur: string) => {
+        const id = cur.replace(/\D/g, '');
+        const channel = this.container.guildService.get().channels.get(id) as TextChannel;
 
-      this.container.loggerService.info(`turning on slowmode in ${channel.name}`);
+        channel && acc.push(channel);
 
-      c.send(`**ANNOUNCEMENT**\nSlowmode is on until ${expDate.toISOString()}`);
+        return acc;
+      }, [])
+      .forEach(async (channel: TextChannel) => {
+        this.container.loggerService.info(`turning on slowmode in ${channel.name}`);
 
-      await c.setRateLimitPerUser(+slowmodeSetting, `slowmode command by ${message.author.tag}`);
-      setTimeout(createUndoFunc(c), 1000 * +expiration);
-    }
+        channel.send(`**ANNOUNCEMENT**\nSlowmode is on until ${expDate.toISOString()}`);
+
+        // turn on slow mode
+        await channel.setRateLimitPerUser(
+          +slowmodeSetting,
+          `slowmode command by ${message.author.tag}`
+        );
+
+        // create delayed-call to undo slowmode.
+        setTimeout(createUndoFunc(channel), 1000 * +expiration);
+      });
   }
 }
