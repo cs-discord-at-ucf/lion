68dccf3bd50e53491ca2be6479ffb9161c013de2
gooca <nicholasjalemao@gmail.com>
2020-10-10 22:02:13 -0400

    Add role.service and role check (#199)

--- src/services/role.service.ts
+++ src/services/role.service.ts
@@ -1,1 +1,23 @@
-export class RoleService {}
+import { RoleType, RoleTypeKey } from './../common/types';
+import { GuildMember, Role, Collection } from 'discord.js';
+
+export class RoleService {
+  hasPermission(member: GuildMember, minRoleToRun: number) {
+    const roles = member.roles;
+    const highestRole = this._getHighestRole(roles);
+    return highestRole >= minRoleToRun;
+  }
+
+  private _getHighestRole(roles: Collection<string, Role>) {
+    let highestRole = 0;
+
+    roles.forEach((role: Role) => {
+      if (role.name in RoleType) {
+        if (role.name === 'Suspended') return -10;
+        highestRole = Math.max(highestRole, RoleType[<RoleTypeKey>role.name]);
+      }
+    });
+
+    return highestRole;
+  }
+}
