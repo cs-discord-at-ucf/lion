a02ee976073188eb7c24e3c85d1c5022976cf4e6
LemensRUs <72531598+LemensRUs@users.noreply.github.com>
2020-10-12 16:39:37 -0400

    Created MarketPlacePlugin

---
+++ src/app/plugins/marketplace.plugin.ts
@@ -0,0 +1,70 @@
+import Constants from '../../common/constants';
+import { Plugin } from '../../common/plugin';
+import { IContainer, IMessage, ChannelType } from '../../common/types';
+import { RichEmbed } from 'discord.js';
+
+export class MarketPlacePlugin extends Plugin {
+  public name: string = 'MarketPlace';
+  public description: string = 'Stores and Lists Everything On MarketPlace.';
+  public usage: string = '!Market <add/list>';
+  public pluginAlias = [];
+  public permission: ChannelType = ChannelType.Public;
+  public pluginChannelName: string = Constants.Channels.Public.BuySellTrade;
+
+  constructor(public container: IContainer) {
+    super();
+  }
+
+  public async execute(message: IMessage, args: string[]) {
+    if (!args) {
+      message.reply('Invalid syntax.');
+      return;
+    }
+
+    const [sub_command] = args; //set sub command = args[0].
+    const description = message.content.slice(17); //everything after the first 17 letters is the listing.
+
+    if (sub_command === 'add') {
+      this._handleAddMarket(message, description);
+    } else if (sub_command === 'list') {
+      this._handleListMarket(message);
+    } else {
+      message.reply('Invalid command. See !help');
+    }
+  }
+
+  private _handleAddMarket(message: IMessage, description: string) {
+    if (description !== '') {
+      message.reply('Your item has been added! Please delete your message once it is sold.');
+    } else {
+      message.reply('Invalid Listing');
+    }
+  }
+
+  private _handleListMarket(message: IMessage) {
+    // Get messages.
+    message.channel.fetchMessages({ limit: 100 }).then((msgs) => {
+      const itemsForSale: String[] = [];
+
+      //iterate over messages.
+      msgs.forEach(function (msg) {
+        const content = msg.content;
+
+        if (content.includes('!marketplace add')) {
+          let item = content.slice(17); //everything after the first 17 letters is the listing.
+          if (item !== '') {
+            const user = msg.author;
+            item += '\n' + user; //adds user to end of listing.
+            itemsForSale.unshift(item);
+          }
+        }
+      });
+
+      const embed = new RichEmbed();
+      embed.setTitle('Items For Sale');
+      embed.setColor('#7289da');
+      embed.setDescription(itemsForSale.join('\n\n'));
+      message.author.send(embed);
+    });
+  }
+}
