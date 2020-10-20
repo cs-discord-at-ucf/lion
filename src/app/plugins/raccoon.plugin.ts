2e994d1152766dd9d4220e709ecf56f8c8622876
zenith110 <thequake@live.com>
2020-10-19 21:53:47 -0400

    Polished up the raccoon plugin

--- src/app/plugins/raccoon.plugin.ts
+++ src/app/plugins/raccoon.plugin.ts
@@ -3,8 +3,6 @@ import { Plugin } from '../../common/plugin';
 import { ChannelType, IContainer, IMessage, IHttpResponse} from '../../common/types';
 import { RichEmbed } from 'discord.js';
 
-
-
 export class RaccoonPlugin extends Plugin {
   public name: string = 'Raccoon Plugin';
   public description: string = 'Generates pictures of raccoons.';
@@ -14,26 +12,26 @@ export class RaccoonPlugin extends Plugin {
   public pluginChannelName: string = Constants.Channels.Public.Pets;
 
   private _API_URL: string = 'https://fetchit.dev/raccoon/';
-  private _embedRacoon = new RichEmbed();
-
   constructor(public container: IContainer) {
     super();
   }
+
   public async execute(message: IMessage, args?: string[]) {
+    try {
     await this.container.httpService
       .get(this._API_URL)
       .then((response: IHttpResponse) => {
-            this._embedRacoon.setTitle("Raccoon spotted!");
-            this._embedRacoon.setColor('#7289da');
-            this._embedRacoon.setImage(response["data"]["raccoon"][0]["Image"])
-            this._embedRacoon.setFooter("And just like that, he's gone");
-            message.channel.send(this._embedRacoon)
-      })
+            const embedRacoon: RichEmbed = new RichEmbed();
+            embedRacoon.setTitle("Raccoon spotted!");
+            embedRacoon.setColor('#7289da');
+            embedRacoon.setImage(response["data"]["raccoon"][0]["Image"]);
+            embedRacoon.setFooter("And just like that, he's gone");
+            message.reply(embedRacoon);
+        })
       .catch((err) => this.container.loggerService.warn(err));
-  }
-
-  // gets the commands and puts spaces between all words
-  private _parseCommand(args: string[]): string {
-    return args.map((str) => str.toLowerCase()).join(' ');
+    }
+  catch(err) {
+    this.container.loggerService.warn(err);
+    }
   }
 }
