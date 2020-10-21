de56876c6edfb5e17d029160c4bd34f8d04b6419
Abrahan N <35606009+zenith110@users.noreply.github.com>
2020-10-21 14:40:21 -0400

    Racoon plugin (#201)
    * Finished the raccon plugin
    
    Co-authored-by: c650 <c650@users.noreply.github.com>

--- src/bootstrap/plugin.loader.ts
+++ src/bootstrap/plugin.loader.ts
@@ -20,6 +20,7 @@ import { ModReportPlugin } from '../app/plugins/modreport.plugin';
 import { ChanBanPlugin } from '../app/plugins/chanban.plugin';
 import { SlowModePlugin } from '../app/plugins/slowmode.plugin';
 import { ScoresPlugin } from '../app/plugins/scores.plugin';
+import {RaccoonPlugin} from '../app/plugins/raccoon.plugin';
 import { ManageRolesPlugin } from '../app/plugins/manageroles.plugin';
 
 const PluginStore: { [pluginName: string]: any } = {
@@ -45,6 +46,7 @@ const PluginStore: { [pluginName: string]: any } = {
   chanban: ChanBanPlugin,
   slowmode: SlowModePlugin,
   scores: ScoresPlugin,
+  raccoon: RaccoonPlugin,
   manageroles: ManageRolesPlugin,
 };
 
