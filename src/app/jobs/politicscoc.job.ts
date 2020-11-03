ad9a587f0fe3c3f38a4c8c625ed6fe126981c9cb
c650 <c650@users.noreply.github.com>
2020-11-03 10:17:58 -0500

    create job to remind of code of conduct in pol

---
+++ src/app/jobs/politicscoc.job.ts
@@ -0,0 +1,51 @@
+import { IContainer, Mode } from '../../common/types';
+import { Job } from '../../common/job';
+import Constants from '../../common/constants';
+import { TextChannel } from 'discord.js';
+
+export class PoliticsCoCReminder extends Job {
+  public interval: number = 1000 * 60 * 30; // every 30 minutes
+  public name: string = 'politics_coc_reminder';
+
+  constructor() {
+    super();
+  }
+
+  public execute(container: IContainer) {
+    try {
+      container.loggerService.debug(`Starting ${this.name} job`);
+
+      if (!container.guildService.get()) {
+        container.loggerService.warn('No guild yet');
+        return;
+      }
+
+      // console.log(container.guildService.get().channels);
+
+      const politicsChan = container.guildService
+        .get()
+        .channels.find((c) => c.name === Constants.Channels.Public.Politics);
+
+      if (!politicsChan) {
+        container.loggerService.silly("no politics channel detected (it's for the best)");
+        return;
+      }
+
+      const codeOfConduct = container.guildService
+        .get()
+        .channels.find((c) => c.name === Constants.Channels.Public.CodeOfConduct);
+
+      if (!codeOfConduct) {
+        container.loggerService.silly('no code of conduct channel detected');
+        return;
+      }
+
+      (politicsChan as TextChannel)?.send(
+        `Please remember to follow the <#${codeOfConduct.id}>, especially in this channel!`
+      );
+    } catch (ex) {
+      container.loggerService.error(ex);
+      console.log(ex);
+    }
+  }
+}
