afd464bc68617c9d09a3ced26faed44ddb17f016
Tanndlin <tanndlin@gmail.com>
2020-11-06 14:38:05 -0500

    Get latest Commit

--- src/app/plugins/status.plugin.ts
+++ src/app/plugins/status.plugin.ts
@@ -19,6 +19,57 @@ export class StatusPlugin extends Plugin {
   }
 
   public async execute(message: IMessage, args: string[]) {
-    console.log('Test!');
+    const latestCommit = await Promise.resolve(this._getLatestCommit());
+    console.log(latestCommit);
+  }
+
+  private async _getLatestCommit() {
+    const result = (await this._execute('git log')) as string;
+    const commits = result.split('commit').slice(1); //First element is an empty string
+    const latestCommit = this._parseCommit(commits[0]);
+
+    return latestCommit;
+  }
+
+  //Returns object containing [commitNumber, author, date]
+  private _parseCommit(data: string) {
+    const parsedData = data.split('\n').filter((e) => e != '');
+    const [commitNumber, author, date, ...commits] = parsedData;
+    const usernameRegex: RegExp = / [a-zA-Z0-9]+ /;
+
+    const authorMatch = author.match(usernameRegex);
+    if (!authorMatch) {
+      return;
+    }
+
+    const parsedAuthor = authorMatch[0].trim();
+    const parsedCommits = commits.map((e) => e.trim());
+
+    const commitData = {
+      number: commitNumber.trim(),
+      author: parsedAuthor,
+      date: date.split('   ')[1], //the data looks like this 'Date:   Fri Nov...'
+      commits: parsedCommits,
+    };
+
+    return commitData;
+  }
+
+  private _execute(command: string) {
+    const { exec } = require('child_process');
+    return new Promise(function(resolve, reject) {
+      exec(command, (error: Error, stdout: string, stderr: string) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        if (stderr) {
+          reject(stderr);
+          return;
+        }
+
+        resolve(stdout.trim());
+      });
+    });
   }
 }
