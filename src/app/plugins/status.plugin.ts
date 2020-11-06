89924f90d677d2b00afc1bb52f11c5c410a28f13
Tanndlin <tanndlin@gmail.com>
2020-11-06 15:59:55 -0500

    Refactor

--- src/app/plugins/status.plugin.ts
+++ src/app/plugins/status.plugin.ts
@@ -55,10 +55,6 @@ export class StatusPlugin extends Plugin {
     const minutes = Math.floor((duration / (1000 * 60)) % 60);
     const hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
 
-    // hours = hours < 10 ? '0' + hours : hours;
-    // minutes = minutes < 10 ? '0' + minutes : minutes;
-    // seconds = seconds < 10 ? '0' + seconds : seconds;
-
     return hours + ':' + minutes + ':' + seconds;
   }
 
