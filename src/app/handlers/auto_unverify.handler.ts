2ffcd7ccf8b4412502316a3250594faf91e285e2
Tanndlin <tanndlin@gmail.com>
2020-11-08 18:25:00 -0500

    Changed member fetch

--- src/app/handlers/auto_unverify.handler.ts
+++ src/app/handlers/auto_unverify.handler.ts
@@ -10,10 +10,7 @@ export class AutoUnverifyHandler implements IHandler {
     }
 
     const unverifiedRole = message.guild.roles.filter((role) => role.name === 'Un verified');
-    const targetGuildMember = message.guild.members
-      .filter((user) => user.id === message.author.id)
-      .first();
-
-    targetGuildMember.addRoles(unverifiedRole);
+    const member = await this.container.guildService.get().fetchMember(message.author.id);
+    member.addRoles(unverifiedRole);
   }
 }
