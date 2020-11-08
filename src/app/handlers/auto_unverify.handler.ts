088be2557581254291dbc3c638cd5b18730ed464
Tanndlin <tanndlin@gmail.com>
2020-11-08 18:01:34 -0500

    Refactor

--- src/app/handlers/auto_unverify.handler.ts
+++ src/app/handlers/auto_unverify.handler.ts
@@ -4,15 +4,16 @@ export class AutoUnverifyHandler implements IHandler {
   constructor(public container: IContainer) {}
 
   public async execute(message: IMessage): Promise<void> {
-    const avatar = message.author.avatar; //This is null if they have default avatar
-
-    if (!avatar) {
-      const unverifiedRole = message.guild.roles.filter((role) => role.name === 'Un verified');
-      const targetGuildMember = message.guild.members
-        .filter((user) => user.id === message.author.id)
-        .first();
-
-      targetGuildMember.addRoles(unverifiedRole);
+    const hasAvatar = Boolean(message.author.avatar);
+    if (hasAvatar) {
+      return;
     }
+
+    const unverifiedRole = message.guild.roles.filter((role) => role.name === 'Un verified');
+    const targetGuildMember = message.guild.members
+      .filter((user) => user.id === message.author.id)
+      .first();
+
+    targetGuildMember.addRoles(unverifiedRole);
   }
 }
