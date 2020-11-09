7bde8de5982e8aac1055baa396beda44ce3db3e6
c650 <c650@users.noreply.github.com>
2020-11-08 20:26:25 -0500

    DM Anon Reports (#223)
    * creates DM anon reports
    
    * deepscan
    
    * catch promise

---
+++ src/app/plugins/anonreport.plugin.ts
@@ -0,0 +1,26 @@
+import { Plugin } from '../../common/plugin';
+import { IContainer, IMessage, ChannelType } from '../../common/types';
+
+export class DmReportPlugin extends Plugin {
+  public name: string = 'anonreport';
+  public description: string = 'anonymously report a concern to moderation team';
+  public usage: string = 'simply DM lion, start message with !anonreport and write your concern';
+  public pluginAlias = [];
+  public permission: ChannelType = ChannelType.Public;
+
+  public usableInDM = true;
+  public usableInGuild = false;
+
+  constructor(public container: IContainer) {
+    super();
+  }
+
+  public async execute(message: IMessage, args?: string[]) {
+    this.container.modService
+      .fileAnonReport(message)
+      .then(() => {
+        message.reply('Thank you, your report has been recorded.');
+      })
+      .catch((e) => this.container.loggerService.error(e));
+  }
+}
