b4b28e8790bf9c62b1c0f6d8b133450634a75dd3
Tanner Sandlin <tanndlin@gmail.com>
2020-11-24 13:25:39 -0500

    Register multiple classes at once (#233)
    * Register multiple classes at once
    
    * Continued after build error
    
    * Specific message for no classes added
    
    * Only Send error once
    
    * Log Error
    
    * Deepscan

--- src/app/plugins/register.plugin.ts
+++ src/app/plugins/register.plugin.ts
@@ -17,16 +17,40 @@ export class RegisterPlugin extends Plugin {
   }
 
   public async execute(message: IMessage, args?: string[]) {
-    const request = this.container.classService.buildRequest(message.author, args);
-    if (!request) {
-      message.reply('I was unable to build your request.');
+    if (!args) {
       return;
     }
-    try {
-      const response = await this.container.classService.register(request);
-      message.reply(response);
-    } catch (e) {
-      message.reply(e);
+
+    const successfulClasses: string[] = [];
+    const invalidClasses: string[] = [];
+    for (const arg of args) {
+      const request = this.container.classService.buildRequest(message.author, [arg]);
+      if (!request) {
+        invalidClasses.push(arg);
+        continue;
+      }
+      try {
+        const response = await this.container.classService.register(request);
+        if (response.includes('success')) {
+          successfulClasses.push(arg);
+        } else {
+          invalidClasses.push(arg);
+        }
+      } catch (e) {
+        this.container.loggerService.error(e);
+      }
     }
+
+    let messageForUser;
+    if (successfulClasses.length === 0) {
+      messageForUser = 'No classes successfully added.';
+    } else {
+      messageForUser = `Successfully added to ${successfulClasses.length} classes`;
+    }
+
+    if (invalidClasses.length > 0) {
+      messageForUser += `\nUnable to locate these classes: ${invalidClasses.join(' ')}`;
+    }
+    message.reply(messageForUser);
   }
 }
