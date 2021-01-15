7b53f9da17e62780edbc21e9d219f5337bc86b59
Tanndlin <tanndlin@gmail.com>
2021-01-14 19:09:54 -0500

    Allow multiple unregister

--- src/app/plugins/unregister.plugin.ts
+++ src/app/plugins/unregister.plugin.ts
@@ -1,5 +1,5 @@
 import { Plugin } from '../../common/plugin';
-import { IContainer, IMessage, ChannelType } from '../../common/types';
+import { IContainer, IMessage, ChannelType, IClassRequest } from '../../common/types';
 
 export class UnregisterPlugin extends Plugin {
   public name: string = 'Unregister Plugin';
@@ -13,20 +13,60 @@ export class UnregisterPlugin extends Plugin {
   }
 
   public validate(message: IMessage, args: string[]) {
-    return !!args.length;
+    return args.length > 0;
   }
 
-  public async execute(message: IMessage, args?: string[]) {
-    const request = this.container.classService.buildRequest(message.author, args);
-    if (!request) {
-      message.reply('I was unable to build your request.');
+  public async execute(message: IMessage, args: string[]) {
+    if (args[0].toLowerCase() === 'all') {
+      await this._removeFromAllClasses(message);
       return;
     }
-    try {
-      const response = await this.container.classService.unregister(request);
-      message.reply(response);
-    } catch (e) {
-      message.reply(e);
+
+    const successfulClasses: string[] = [];
+    const invalidClasses: string[] = [];
+
+    for (const arg of args) {
+      const request = this.container.classService.buildRequest(message.author, [arg]);
+      if (!request) {
+        invalidClasses.push(arg);
+        continue;
+      }
+
+      try {
+        const response = await this.container.classService.unregister(request);
+        if (response.includes('success')) {
+          successfulClasses.push(arg);
+        } else {
+          invalidClasses.push(arg);
+        }
+      } catch (e) {
+        this.container.loggerService.error(e);
+      }
     }
+
+    let messageForUser;
+    if (successfulClasses.length === 0) {
+      messageForUser = 'No classes successfully removed.';
+    } else {
+      messageForUser = `Successfully removed from ${successfulClasses.length} classes`;
+    }
+
+    if (invalidClasses.length > 0) {
+      messageForUser += `\nUnable to locate these classes: ${invalidClasses.join(' ')}`;
+    }
+    message.reply(messageForUser);
+  }
+
+  private async _removeFromAllClasses(message: IMessage) {
+    const request = this.container.classService.buildRequest(message.author, ['all']);
+    if (!request) {
+      message.reply('Unable to complete your request.');
+      return;
+    }
+
+    const reponse = await this.container.classService.unregister(request);
+    console.log(reponse);
+
+    message.reply('Successfully removed from all classes');
   }
 }
