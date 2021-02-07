f4e9dbcfb842cf44e1fab4c0f83fea3cab6e94a9
Tanndlin <tanndlin@gmail.com>
2021-02-07 11:19:02 -0500

    Moved Functions to other services and jobs

--- src/app/plugins/createclassvoice.plugin.ts
+++ src/app/plugins/createclassvoice.plugin.ts
@@ -1,6 +1,6 @@
 import { Plugin } from '../../common/plugin';
-import { IContainer, IMessage, ChannelType, Maybe } from '../../common/types';
-import { CategoryChannel, TextChannel } from 'discord.js';
+import { IContainer, IMessage, ChannelType } from '../../common/types';
+import { TextChannel, VoiceChannel } from 'discord.js';
 
 export class CreateClassVoice extends Plugin {
   public name: string = 'Create Class Voice';
@@ -8,9 +8,6 @@ export class CreateClassVoice extends Plugin {
   public usage: string = 'createclassvoice';
   public pluginAlias = [];
   public permission: ChannelType = ChannelType.Private;
-
-  private _INACTIVE_THRESHOLD: number = 10;
-  private _AUDIO_CAT: Maybe<CategoryChannel> = null;
 
   constructor(public container: IContainer) {
     super();
@@ -22,39 +19,26 @@ export class CreateClassVoice extends Plugin {
 
   public async execute(message: IMessage, args: string[]) {
     const chan = message.channel as TextChannel;
-    if (!this._AUDIO_CAT) {
-      this._AUDIO_CAT = this.container.guildService
-        .get()
-        .channels.cache.filter((c) => c.name === 'Audio Channels')
-        .first() as CategoryChannel;
+    const voiceChan = await this.container.classService.createVoiceChan(chan);
+    if (!voiceChan) {
+      await message.reply('There is already a voice channel for this class');
+      return;
     }
-
-    const voiceChan = await this.container.guildService.get().channels.create(chan.name, {
-      type: 'voice',
-      parent: this._AUDIO_CAT,
-      permissionOverwrites: chan.permissionOverwrites,
-    });
 
     await voiceChan.createInvite().then(async (invite) => {
       await message.channel.send(`Channel Created: ${invite}`);
     });
+  }
+}
 
-    let numInChan = voiceChan.members.size;
-    const deleteInterval = setInterval(async () => {
-      const newAmount = voiceChan.members.size;
+export class ClassVoiceChan {
+  voiceChan: VoiceChannel;
+  classChan: TextChannel;
+  lastUsers: number;
 
-      //If there was nobody last check and no one this check
-      //Remove the channel
-      if (numInChan === newAmount && newAmount === 0) {
-        await voiceChan.delete(`Inactive for ${this._INACTIVE_THRESHOLD} minutes`);
-
-        this.container.loggerService.info(
-          `Voice channel for #${chan.name} was deleted. Inactive for ${this._INACTIVE_THRESHOLD} minutes`
-        );
-        clearInterval(deleteInterval);
-      } else {
-        numInChan = newAmount;
-      }
-    }, 1000 * 60 * this._INACTIVE_THRESHOLD); //Check every _INACTIVE_THRESHOLD minutes
+  constructor(_chan: VoiceChannel, _classChan: TextChannel, _numUsers: number) {
+    this.voiceChan = _chan;
+    this.classChan = _classChan;
+    this.lastUsers = _numUsers;
   }
 }
