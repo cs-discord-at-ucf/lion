f68a25ac0f1fd5a02247eeedf04eae48bc1c60d9
Callum <Callum>
2021-03-24 15:54:30 -0400

    Made channel redirection a bit easier to understand.

--- src/common/plugin.ts
+++ src/common/plugin.ts
@@ -1,4 +1,6 @@
 import { ChannelType, IContainer, IMessage, IPlugin, RoleType } from './types';
+import Constants from '../common/constants';
+import { Container } from 'winston';
 
 export abstract class Plugin implements IPlugin {
   public abstract container: IContainer;
@@ -28,7 +30,15 @@ export abstract class Plugin implements IPlugin {
   public hasPermission(message: IMessage): boolean {
     const channelName = this.container.messageService.getChannel(message).name;
     if (typeof this.pluginChannelName === 'string' && this.pluginChannelName !== channelName) {
-      message.reply(`Please use this command in the \`#${this.pluginChannelName}\` channel.`);
+
+      //if this fails it simply means that a channel the bot expected to exist didn't, nothing bot breaking
+      try{
+        const id = this.container.guildService.getChannel(this.pluginChannelName).id
+        message.reply(`Please use this command in the <#${id}> channel.`);
+      }catch(err){
+        this.container.loggerService.warn(err)
+        message.reply(`Please use this command in the \`#${this.pluginChannelName}\` channel.`);
+      }
       return false;
     }
 
@@ -47,7 +57,29 @@ export abstract class Plugin implements IPlugin {
 
     const response = this.container.channelService.hasPermission(channelName, this.permission);
     if (!response) {
-      message.reply(`Please use this command in a \`${this.permission}\` channel`);
+      const rooms = Object.values(Constants.Channels[this.permission]);
+      let addonText = '';
+
+      //if this fails it simply means that a channel the bot expected to exist didn't
+      try{
+        if (rooms.length === 0) {
+          addonText = "their are no channels oh this type declared";
+        } else if (rooms.length === 1) {
+          let id = this.container.guildService.getChannel(rooms[0]).id
+          addonText = `, <#${id}> is the only channel whith this type`
+        } else{
+          let id = [this.container.guildService.getChannel(rooms[0]).id, this.container.guildService.getChannel(rooms[1]).id]
+          if (rooms.length == 2){
+            addonText = `, <#${id[0]}> and <#${id[1]}> are the only two with this channel type`
+          } else {
+            addonText = `, <#${id[0]}> and <#${id[1]}> are two of the channels this channel type`
+          }
+        }
+      } catch(err){
+        this.container.loggerService.warn(err)
+      } finally {
+        message.reply(`Please use this command in a \`${this.permission}\` channel${addonText}.`);
+      }
     }
     return response;
   }
