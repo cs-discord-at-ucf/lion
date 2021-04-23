22015a80cc8686749c1ab73403d3c7cea592b911
CallumCoy <callumcoy@ymail.com>
2021-04-23 00:59:26 -0400

    requested changes

--- src/app/plugins/pubsub.plugin.ts
+++ src/app/plugins/pubsub.plugin.ts
@@ -13,19 +13,18 @@ export class PubSubPlugin extends Plugin {
 
   private _API_URL: string = 'https://api.pubsub-api.dev';
   private _VALID_KEYS: string[] = ['random', 'list', 'types', ''];
-  private _DEFAULT_INPUT: string = 'chicken-tenders';
   private _SUBS: string[] = [];
   private _EMBED_LIST = new MessageEmbed();
 
   private _SUB_UPD_THRESH: number = 1000 * 60 * 60 * 24; // in ms, one day.
-  private _LAST_UPD_TIME: number = 0;
+  private _LAST_UPD_TIME: number = -this._SUB_UPD_THRESH - 1;
 
   constructor(public container: IContainer) {
     super();
     this._updateList();
   }
 
-  private async _updateList() {
+  private _updateList() {
     this.container.httpService
       .get(`${this._API_URL}/allsubs/`)
       .then((response: IHttpResponse) => {
@@ -37,24 +36,52 @@ export class PubSubPlugin extends Plugin {
   }
 
   public async execute(message: IMessage, args?: string[]) {
-    const input = (args || []).join('-').toLowerCase() || this._DEFAULT_INPUT;
+    const subIn = this._parseInput(args || []);
 
-    if (input === 'list' || input === 'types') {
+    // return the og message
+    if (!subIn) {
+      const state = this.container.storeService.get('pubSubStore').state;
+      if (state?.onSale) {
+        message.reply('Chicken tender subs are on sale :eyes:');
+      } else {
+        message.reply('Chicken tender subs are **NOT** on sale.');
+      }
+      return;
+    }
+
+    if (subIn === 'list' || subIn === 'types') {
       //Simply return the list of supported subs
-      await this._generateEmbedList();
+      this._generateEmbedList();
       message.reply(this._EMBED_LIST);
       return;
     }
 
     // checks if their sub was a sub, then if that sub is recognised
-    const subType = this._SUBS.find((sub: string) => sub === input) || 'random';
+    const subType = this._SUBS.find((sub: string) => sub === subIn) || 'random';
 
     //recieves the according info and posts, or derps
     await this.container.httpService
       .get(`${this._API_URL}/subs/?name=${subType}`)
       .then((response: IHttpResponse) => {
         const [subData] = response.data;
-        const embed: MessageEmbed = this._generateEmbedSub(subData);
+        const embed = new MessageEmbed();
+
+        const parsedNamed = this._normaliseName(subData.sub_name);
+        const saleInfo =
+          subData.status.toLowerCase() === 'true' //a function would be too shallow to warrant making
+            ? `for the discounted price of ${subData.price} until ${
+                subData.last_sale.split('-')[1]
+              }`
+            : `it isn't on sale currently, but who could say no to ${subData.price}.  The last time it was on sale was ${subData.last_sale}`;
+
+        embed.setColor('#0099ff').setTitle(parsedNamed);
+
+        embed.setDescription(
+          `Get your own *${parsedNamed}* sub, ${saleInfo}. So what are you waiting for?  Come on down to Publix now to get yourself a beautiful sub. Just look at this beauty right here! 😍😍😍:pubsub::pubsub::pubsub:🤤🤤🤤` +
+            `\n\n *pulled from https://pubsub-api.dev.*`
+        );
+
+        embed.setImage(subData.image);
 
         message.reply(embed);
       })
@@ -63,31 +90,25 @@ export class PubSubPlugin extends Plugin {
 
   private async _generateEmbedList(): Promise<void> {
     const lastListUpdate: number = Date.now() - this._LAST_UPD_TIME;
-
     if (lastListUpdate < this._SUB_UPD_THRESH) {
       return;
     }
 
     this._LAST_UPD_TIME = Date.now(); //since it only updates once a day, don't need to worry about accuracy
 
-    await this._updateList();
+    this._updateList();
 
-    //setting up number of columns and rows for the list
-    const maxCol = 3;
-    const maxRows = 10;
-    const numCols = Math.min(maxCol, Math.ceil(this._SUBS.length / maxRows));
+    const numCols = Math.min(3, Math.ceil(this._SUBS.length / 10));
     const numRows = Math.ceil(this._SUBS.length / numCols);
 
-    //sets up a new embed for the list to reside in
     this._EMBED_LIST = new MessageEmbed();
-    this._EMBED_LIST.setColor('#0099ff').setTitle('Types of Subs');
-    const subs = this._SUBS.map((sub) => this._normalizeName(sub));
 
-    //cycles through each column inserting their row, and also notes the alphabetic range
+    this._EMBED_LIST.setColor('#0099ff').setTitle('Types of Subs');
+    const subs = this._SUBS.map((sub) => this._normaliseName(sub));
+
     for (let cols = 0; cols < numCols; cols++) {
       const columnSubs = subs.slice(numRows * cols, numRows * (cols + 1));
 
-      //each column is a embed field note: only 3 fields can be lined up
       this._EMBED_LIST.addField(
         `${columnSubs[0].charAt(0)} - ${columnSubs[columnSubs.length - 1].charAt(0)}`,
         columnSubs.join('\n'),
@@ -96,45 +117,20 @@ export class PubSubPlugin extends Plugin {
     }
   }
 
-  private _generateEmbedSub(subData: any): MessageEmbed {
-    const embed: MessageEmbed = new MessageEmbed();
-    const parsedNamed = this._normalizeName(subData.sub_name);
-
-    //defaults to not on sale
-    let saleInfo = `it isn't on sale currently, but who could say no to ${subData.price}.  The last time it was on sale was ${subData.last_sale}`;
-
-    //status is true if the sub is on sale
-    if (subData.status.toLowerCase() === 'true') {
-      saleInfo = `for the discounted price of ${subData.price} until ${
-        subData.last_sale.split('-')[1]
-      }`;
-    }
-
-    embed.setColor('#0099ff').setTitle(parsedNamed + ' sub');
-
-    embed.setDescription(
-      `Get your own *${parsedNamed}* sub, ${saleInfo}.` +
-        ` So what are you waiting for?  Come on down to Publix now to get yourself a beautiful sub.` +
-        ` Just look at this beauty right here! 😍😍😍:pubsub::pubsub::pubsub:🤤🤤🤤`
-    );
-
-    embed.setImage(subData.image);
-
-    embed.setFooter(`pulled from: pubsub-api.dev.`);
-
-    return embed;
-  }
-
   public validate(message: IMessage, args?: string[]) {
-    const input = (args || []).join('-').toLowerCase();
+    const subIn = this._parseInput(args || []);
 
-    const keywordCheck: boolean = this._VALID_KEYS.includes(input);
-    const subTypeCheck: boolean = this._SUBS.includes(input);
+    const keywordCheck: boolean = this._VALID_KEYS.includes(subIn);
+    const subTypeCheck: boolean = this._SUBS.includes(subIn);
 
     return subTypeCheck || keywordCheck;
   }
 
-  private _normalizeName(input: string): string {
+  private _parseInput(args: string[]): string {
+    return args.join('-').toLowerCase();
+  }
+
+  private _normaliseName(input: string): string {
     return input
       .replace(/-/g, ' ') // adds spaces
       .replace(/\b\w/g, (l: string) => l.toUpperCase()); // adds capitalization
