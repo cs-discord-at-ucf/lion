2751619ff39613212112ef58592a3f8bd0337af7
Tanndlin <tanndlin@gmail.com>
2021-05-02 01:47:35 -0400

    req mod to queue

--- src/app/plugins/queue.plugin.ts
+++ src/app/plugins/queue.plugin.ts
@@ -1,5 +1,6 @@
 import { Plugin } from '../../common/plugin';
 import { IContainer, IMessage, ChannelType } from '../../common/types';
+import { MemberUtils } from '../util/member.util';
 
 export class QueuePlugin extends Plugin {
   public name: string = 'Play Plugin';
@@ -13,6 +14,16 @@ export class QueuePlugin extends Plugin {
   }
 
   public async execute(message: IMessage, args: string[]) {
+    if (!message.member) {
+      return;
+    }
+
+    const isModerator = MemberUtils.hasRole(message.member, 'moderator');
+    if (!isModerator) {
+      await message.reply('You must be a moderator to use this plugin.');
+      return;
+    }
+
     const res = this.container.musicService.listQueue();
     await message.reply(res);
   }
