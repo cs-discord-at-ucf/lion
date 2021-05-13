aa39cccb526dc403e1baf9793dada582bc8348f2
Callum Coy <callumcoy@ymail.com>
2021-05-13 13:33:49 -0400

    Dog update (#373)
    * New dog API
    
    * removed validate
    
    * made some functions private
    
    * removed a poitnless line
    
    * reversed a if statement
    
    * added a interface
    
    * REmoved async from some functions
    
    * fixed bug from when I removed validate
    
    * Fixed Spelling mistake, and space.
    
    * Made _allBreeds a Set, Also changed how allBreeds is formed.
    
    * tiny comment changes
    
    * fixed usage standard
    
    * changed how _subbreeds is formed
    
    * grammar fix
    
    * Added extra feature where you can see if a specific breed has any sub-breeds.
    
    * Major bug fix, generateEmbedList would empty the send array.
    
    * Missed a line from testing
    
    * changing wording
    
    * changing usage again
    
    * changed a line that was editing a pattern.

--- src/app/plugins/dog.plugin.ts
+++ src/app/plugins/dog.plugin.ts
@@ -1,66 +1,159 @@
+import { MessageEmbed } from 'discord.js';
 import Constants from '../../common/constants';
 import { Plugin } from '../../common/plugin';
-import { ChannelType, IContainer, IHttpResponse, IMessage } from '../../common/types';
+import { ChannelType, IContainer, IHttpResponse, IMessage, Maybe } from '../../common/types';
 
 export class DogPlugin extends Plugin {
   public name: string = 'Dog Plugin';
   public description: string = 'Generates pictures of doggos.';
-  public usage: string = 'dog <breed>';
+  public usage: string =
+    'dog <subreed (Optional)>  <breed (Optional)> | dog listBreeds | dog listSubBreeds <breed (Optional)>';
   public pluginAlias = ['dogs', 'doggo'];
   public permission: ChannelType = ChannelType.Public;
   public pluginChannelName: string = Constants.Channels.Public.Pets;
 
-  private _API_URL: string = 'https://api.woofbot.io/v1/';
+  private _API_URL: string = 'https://dog.ceo/api/';
+
+  private _allBreeds: Set<string> = new Set([]);
   private _breeds: string[] = [];
+  private _subBreeds: _IDogSubBreed[] = [];
+
+  private _breedEmbed: Maybe<MessageEmbed>;
+  private _subBreedEmbed: Maybe<MessageEmbed>;
 
   constructor(public container: IContainer) {
     super();
     this.container.httpService
-      .get(`${this._API_URL}breeds`)
+      .get(`${this._API_URL}breeds/list/all`)
       .then((response: IHttpResponse) => {
-        const breeds: string[] = response.data.response.breeds;
-        breeds.forEach((breed) => {
-          this._breeds.push(breed.toLowerCase());
+        const breedData = response.data.message;
+
+        this._breeds = Object.keys(breedData);
+        this._allBreeds = new Set(Object.keys(breedData));
+
+        // The json is annoyingly {<breed>: [<subBreeds>]} apposed to {breed: <breed>, subBreed: [<subBreeds>]}} so this gets that
+        this._subBreeds = this._breeds
+          .filter((breed) => breedData[breed].length > 0)
+          .map((breed: string) => {
+            return {
+              breed: breed,
+              subBreed: breedData[breed].map((subBreed: string) => `${subBreed} ${breed}`), // flipped the breed and subBreed
+            };
+          });
+
+        this._subBreeds.forEach((breed) => {
+          breed.subBreed.forEach((subBreed) => this._allBreeds.add(subBreed));
         });
+
+        // Sorting the subBreed list so the embed looks better
+        this._subBreeds.sort((a: any, b: any) => a.subBreed.length - b.subBreed.length);
       })
       .catch((err) => this.container.loggerService.warn(err));
   }
 
   public async execute(message: IMessage, args?: string[]) {
-    let breed = this._parseBreed(args || []);
+    const breed = this._parseInput(args || []);
 
-    if (breed === '' || breed.toLowerCase() === 'random') {
-      breed = this._breeds[this._getRandomInt(this._breeds.length)];
+    if (breed.startsWith('listsubbreeds')) {
+      const breedType = breed.replace('listsubbreeds', '').trim();
+
+      if (!breedType) {
+        await message.reply(this._makeSubBreedEmbed());
+        return;
+      }
+
+      if (this._breeds.includes(breedType)) {
+        await message.reply(this._makeSingleSubBreedEmbed(breedType));
+        return;
+      }
     }
 
-    if (!breed) {
-      this.container.loggerService.warn(`Dog API down.`);
-      await message.reply(`Sorry, the dog plugin has encountered an error.`);
+    if (breed.startsWith('listbreeds')) {
+      await message.reply(this._makeBreedEmbed());
       return;
     }
 
-    if (!this._breeds.includes(breed)) {
-      message.reply(`Breeds supported: \n\`\`\`\n${this._breeds.join('\n')}\`\`\``);
-      return;
+    // The breed and subbreed is reversed for lookup
+    const searchBreed = this._parseInput(args?.reverse() || []).replace(' ', '/');
+    let url = `breed/${searchBreed}/images/random`;
+
+    if (breed === '' || breed === 'random') {
+      url = 'breeds/image/random';
+    } else {
+      // List isn't reversed
+      if (!this._allBreeds.has(breed)) {
+        message.reply(`\`${breed}\` is an invalid breed.`);
+        return;
+      }
     }
 
     await this.container.httpService
-      .get(`${this._API_URL}breeds/${breed}/image`)
+      .get(`${this._API_URL}${url}`)
       .then((response: IHttpResponse) => {
-        if (!!response.data.response.url) {
-          message.reply('', {
-            files: [response.data.response.url],
-          });
+        // Notifies the user if there was a problem contacting the server
+        if (Math.floor(response.status / 100) != 2) {
+          message.reply(
+            `Something seems to have happened with the connection to ${this._API_URL}.`
+          );
+          return;
         }
+
+        message.reply('', {
+          files: [response.data.message],
+        });
       })
-      .catch((err) => this.container.loggerService.warn(err));
+      .catch((err) => {
+        this.container.loggerService.warn(err);
+      });
   }
 
-  private _getRandomInt(max: number): number {
-    return Math.floor(Math.random() * Math.floor(max));
+  private _makeBreedEmbed(): MessageEmbed {
+    if (this._breedEmbed) {
+      return this._breedEmbed;
+    }
+
+    this._breedEmbed = this.container.messageService.generateEmbedList(this._breeds, {
+      title: 'Breeds',
+    });
+
+    return this._breedEmbed;
   }
 
-  private _parseBreed(args: string[]): string {
+  private _makeSubBreedEmbed(): MessageEmbed {
+    if (this._subBreedEmbed) {
+      return this._subBreedEmbed;
+    }
+
+    const embed = new MessageEmbed();
+    embed.setColor('#0099ff').setTitle('Sub Breeds');
+
+    this._subBreeds.forEach((breed) => {
+      embed.addField(breed.breed, breed.subBreed.join('\n'), true);
+    });
+
+    return (this._subBreedEmbed = embed);
+  }
+
+  private _makeSingleSubBreedEmbed(subBreed: string): MessageEmbed | string {
+    const subBreedData = this._subBreeds.find((e) => e.breed === subBreed)?.subBreed;
+
+    if (!subBreedData) {
+      return "This breed doesn't have any sub-breeds.";
+    }
+
+    const embed = new MessageEmbed();
+    embed.setColor('#0099ff').setTitle(subBreed);
+    embed.setDescription(subBreedData.join('\n'));
+
+    return embed;
+  }
+
+  private _parseInput(args: string[]): string {
     return args.map((str) => str.toLowerCase()).join(' ');
   }
+}
+
+interface _IDogSubBreed {
+  breed: string;
+  subBreed: string[];
 }
