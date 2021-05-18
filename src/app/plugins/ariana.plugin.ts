6e2d0f6d5e87ade1a51b292e8faa2923c3b8a56e
Vijay Stroup <vijaystroup@gmail.com>
2021-05-18 18:58:17 -0400

    resolve review comments

--- src/app/plugins/ariana.plugin.ts
+++ src/app/plugins/ariana.plugin.ts
@@ -1,12 +1,12 @@
 import { Plugin } from '../../common/plugin';
-import { IContainer, IMessage, ChannelType, IHttpResponse } from '../../common/types';
+import { IContainer, IMessage, ChannelType, IHttpResponse, Maybe } from '../../common/types';
 
 export class ArianaPlugin extends Plugin {
   public name: string = 'Ariana Plugin';
   public description: string = 'Get a random picture of Ariana Grande.';
   public usage: string = 'ariana';
   public pluginAlias = ['ari'];
-  public permission: ChannelType = ChannelType.Bot;
+  public permission: ChannelType = ChannelType.Public;
 
   private _API_URL: string = 'https://www.gettyimages.com/photos/ariana-grande?page=';
   private reImgs: RegExp = new RegExp(/<img class="gallery-asset__thumb gallery-mosaic-asset__thumb"(.*?)>/g);
@@ -23,13 +23,13 @@ export class ArianaPlugin extends Plugin {
       .get(this._API_URL + '1')
       .then((res: IHttpResponse) => {
         const data: string = res.data;
-        const pagesSearch: RegExpMatchArray | null = data.match(this.rePages);
+        const pagesSearch: Maybe<RegExpMatchArray> = data.match(this.rePages);
 
         if (!pagesSearch) {
           this.container.loggerService.warn('Failed to get Ariana page in constructor.');
           return;
         }
-        const pageSearch: RegExpMatchArray | null = pagesSearch[0].match(this.rePage);
+        const pageSearch: Maybe<RegExpMatchArray> = pagesSearch[0].match(this.rePage);
         if (pageSearch) {
           this.nPages = parseInt(pageSearch[0].replace(/>|</g, ''));
         }
@@ -43,7 +43,7 @@ export class ArianaPlugin extends Plugin {
       .get(this._API_URL + page)
       .then((res: IHttpResponse) => {
         const data: string = res.data;
-        const imgSearch: RegExpMatchArray | null = data.match(this.reImgs);
+        const imgSearch: Maybe<RegExpMatchArray> = data.match(this.reImgs);
 
         if (!imgSearch) {
           return;
@@ -52,11 +52,10 @@ export class ArianaPlugin extends Plugin {
         const count: number = Math.floor(Math.random() * imgSearch.length);
 
         const url = imgSearch[count].match(this.reThumb);
-        if (url) {
-          ari = url[0].replace(/amp;|"/g, '');
-        } else {
+        if (!url) {
           return;
         }
+        ari = url[0].replace(/amp;|"/g, '');
       })
       .catch((err) => this.container.loggerService.warn(err));
 
@@ -64,16 +63,17 @@ export class ArianaPlugin extends Plugin {
   }
 
   public async execute(message: IMessage, args?: string[]) {
-    const page = Math.floor(Math.random() * this.nPages) + 1;
+    const page = Math.floor(Math.random() * this.nPages) + 1; // valid pages 1-100
     const ari = await this.getAri(page);
-    if (ari) {
-      await message.reply('', {
-        files: [{
-          attachment: ari,
-          name: 'ari.jpg'
-        }]});
-      } else {
+    if (!ari) {
       await message.reply('No Ariana <:sadkek:811400946097717328>');
+      return;
     }
+    await message.reply('', {
+      files: [{
+        attachment: ari,
+        name: 'ari.jpg'
+      }]}
+    );
   }
 }
