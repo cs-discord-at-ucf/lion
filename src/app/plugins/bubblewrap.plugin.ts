0efb26e5aa22cba705c199f70f28e622febe72f6
Dylan Ochoa <dylan.ochoa2323@gmail.com>
2021-05-22 19:59:10 -0400

    Bubblewrap plugin ;)

---
+++ src/app/plugins/bubblewrap.plugin.ts
@@ -0,0 +1,19 @@
+import { Plugin } from '../../common/plugin';
+import { IContainer, IMessage, ChannelType } from '../../common/types';
+
+export class BubbleWrapPlugin extends Plugin {
+  public name: string = 'Bubble Wrap';
+  public description: string = 'Sends the user a sheet of bubble wrap to pop';
+  public usage: string = 'bubblewrap';
+  public pluginAlias = ['bw'];
+  public permission: ChannelType = ChannelType.Public;
+
+  constructor(public container: IContainer) {
+    super();
+  }
+
+  public async execute(message: IMessage, args: string[]) {
+    const wrap: string = "||pop||||pop||||pop||||pop||||pop||\n".repeat(5);
+    await message.channel.send(wrap);
+  }
+}
