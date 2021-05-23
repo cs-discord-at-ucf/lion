dad20df3bf410f3789a9a057f5162fb62e53d77c
Tanndlin <tanndlin@gmail.com>
2021-05-23 10:47:38 -0500

    Color Picker Plugin

--- src/bootstrap/plugin.loader.ts
+++ src/bootstrap/plugin.loader.ts
@@ -33,6 +33,7 @@ import { ModHelpPlugin } from '../app/plugins/modhelp.plugin';
 import { CreateClassVoice } from '../app/plugins/createclassvoice.plugin';
 import { PollPlugin } from '../app/plugins/poll.plugin';
 import { BubbleWrapPlugin } from '../app/plugins/bubblewrap.plugin';
+import { ColorPickerPlugin } from '../app/plugins/colorpicker.plugin';
 
 const PluginStore: { [pluginName: string]: any } = {
   dog: DogPlugin,
@@ -70,6 +71,7 @@ const PluginStore: { [pluginName: string]: any } = {
   createclassvoice: CreateClassVoice,
   poll: PollPlugin,
   bubblewrap: BubbleWrapPlugin,
+  colorpicker: ColorPickerPlugin,
 };
 
 export class PluginLoader {
