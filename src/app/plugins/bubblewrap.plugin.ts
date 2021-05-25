2d846ee0e60abcea26653b9649f434ffced5844e
Vijay Stroup <vijaystroup@gmail.com>
2021-05-25 19:34:12 -0400

    Update bubblewrap.plugin.ts

--- src/app/plugins/bubblewrap.plugin.ts
+++ src/app/plugins/bubblewrap.plugin.ts
@@ -13,7 +13,7 @@ export class BubbleWrapPlugin extends Plugin {
   }
 
   public async execute(message: IMessage, args: string[]) {
-    const wrap: string = "||pop||||pop||||pop||||pop||||pop||\n".repeat(5);
+    const wrap: string = "\n" + "||pop||||pop||||pop||||pop||||pop||\n".repeat(5);
     await message.channel.send(wrap);
   }
 }
