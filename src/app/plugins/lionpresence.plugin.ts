6843d1a45258c46a96c7f7af1f211fc2031698dd
Tanndlin <tanndlin@gmail.com>
2021-05-28 20:53:04 -0500

    Fix

--- src/app/plugins/lionpresence.plugin.ts
+++ src/app/plugins/lionpresence.plugin.ts
@@ -5,7 +5,7 @@ export class LionPresence extends Plugin {
   public name: string = 'Lion Presence';
   public description: string = 'Plugin to set the presence of the lion bot.';
   public usage: string = 'setActivity <status>';
-  public pluginAlias = ['setActivity', 'setAct'];
+  public pluginAlias = ['setactivity', 'setact'];
   public permission: ChannelType = ChannelType.Staff;
 
   constructor(public container: IContainer) {
@@ -13,13 +13,18 @@ export class LionPresence extends Plugin {
   }
 
   public validate(message: IMessage, args: string[]) {
-    return args && args.length === 1;
+    return args && args.length >= 1;
   }
 
   public async execute(message: IMessage, args: string[]) {
-    console.log('We executing!');
     await this.container.clientService.user
-      ?.setPresence({ activity: { name: args[1], type: 'CUSTOM_STATUS' }, status: 'idle' })
+      ?.setPresence({
+        activity: {
+          name: args.join(' '),
+          type: 'WATCHING',
+        },
+        status: 'idle',
+      })
       .catch((err) => this.container.loggerService.warn(err));
   }
 }
