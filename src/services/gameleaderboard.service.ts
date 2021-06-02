272f38e40499b4d7d0bc0a4974964ce3c4f49b5e
Armintron <64503268+Armintron@users.noreply.github.com>
2021-06-01 20:06:52 -0400

    class constants

--- src/services/gameleaderboard.service.ts
+++ src/services/gameleaderboard.service.ts
@@ -1,3 +1,21 @@
-import { User } from 'discord.js';
+import { Snowflake, User } from 'discord.js';
+import { StorageService } from './storage.service';
 
-export class GameLeaderboardService {}
+export class GameLeaderboardService {
+  constructor(private _storageService: StorageService) {}
+
+  public async updateLeaderboard(user: User, playerWon: boolean) {
+    const id = user.id;
+
+    const collections = await this._storageService.getCollections();
+    const tttLeaderboard = collections.tttLeaderboard;
+    const connectFourLeaderboard = collections.connectFourLeaderboard;
+  }
+}
+
+export interface GameLeaderBoardEntry {
+  userId: Snowflake;
+  wins: number;
+  loses: number;
+  ties: number;
+}
