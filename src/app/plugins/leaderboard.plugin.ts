ec5b455357d3027a2d7e8427ecbdbddbbff3591d
Armintron <64503268+Armintron@users.noreply.github.com>
2021-06-02 21:40:44 -0400

    deepscan fix

--- src/app/plugins/leaderboard.plugin.ts
+++ src/app/plugins/leaderboard.plugin.ts
@@ -1,4 +1,3 @@
-import { GuildMember, User } from 'discord.js';
 import Constants from '../../common/constants';
 import { Plugin } from '../../common/plugin';
 import { ChannelType, IContainer, IMessage, Maybe } from '../../common/types';
