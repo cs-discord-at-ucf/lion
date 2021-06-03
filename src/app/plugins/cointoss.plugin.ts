9a4b008483279670327eb6478f8162ffb922dbb7
Elbert <etse168@gmail.com>
2021-06-03 10:28:55 -0400

    Added another alias and moved the image variable

--- src/app/plugins/cointoss.plugin.ts
+++ src/app/plugins/cointoss.plugin.ts
@@ -6,10 +6,9 @@ export class CoinToss extends Plugin {
   public name: string = 'Coin Toss Plugin';
   public description: string = 'Ask Lion to toss a coin or if given arguments, choose among the arguments.';
   public usage: string = '!cointoss || !pick <arg_1> <arg_2> ... <arg_n>';
-  public pluginAlias: string[] = ['pick', 'select', 'cointoss', 'toss', 'ct'];
+  public pluginAlias: string[] = ['pick', 'select', 'cointoss', 'coin', 'toss', 'ct'];
   public permission: ChannelType = ChannelType.Public;
   private _embed: MessageEmbed = new MessageEmbed();
-  private _image: string = 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/coin_1fa99.png';
 
   constructor(public container: IContainer) {
     super();
@@ -17,13 +16,15 @@ export class CoinToss extends Plugin {
 
   public async execute(message: IMessage, args?: string[]) {
     let _responses: string[] = [];
+    let _image: string = '';
 
     if (args?.length === 0) {
-      this._embed.setAuthor('Lion flipped a coin and it lands on...', this._image);
+      _image = 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/coin_1fa99.png';
+      this._embed.setAuthor('Lion flipped a coin and it lands on...', _image);
       _responses = ['Heads', 'Tails']
     } else {
-      this._image = 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/magic-wand_1fa84.png';
-      this._embed.setAuthor('Lion has chosen...', this._image);
+      _image = 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/magic-wand_1fa84.png';
+      this._embed.setAuthor('Lion has chosen...', _image);
       _responses = args!;
     }
 
