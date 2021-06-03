0c7e9582b166b68a36105d3a47d4b665ad1ed405
Elbert <etse168@gmail.com>
2021-06-03 10:17:01 -0400

    Added custom argument handling and new aliases

--- src/app/plugins/cointoss.plugin.ts
+++ src/app/plugins/cointoss.plugin.ts
@@ -4,22 +4,32 @@ import { MessageEmbed } from 'discord.js';
 
 export class CoinToss extends Plugin {
   public name: string = 'Coin Toss Plugin';
-  public description: string = 'Ask Lion to toss a coin.';
-  public usage: string = 'toss';
-  public pluginAlias: string[] = ['ct', 'ht', 'coin', 'toss', 'cointoss'];
+  public description: string = 'Ask Lion to toss a coin or if given arguments, choose among the arguments.';
+  public usage: string = '!cointoss || !pick <arg_1> <arg_2> ... <arg_n>';
+  public pluginAlias: string[] = ['pick', 'select', 'cointoss', 'toss', 'ct'];
   public permission: ChannelType = ChannelType.Public;
   private _embed: MessageEmbed = new MessageEmbed();
-  private _IMAGE: string = 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/coin_1fa99.png';
+  private _image: string = 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/coin_1fa99.png';
 
   constructor(public container: IContainer) {
     super();
   }
 
   public async execute(message: IMessage, args?: string[]) {
-    const _responses: string[] = ['Heads', 'Tails'];
+    let _responses: string[] = [];
+
+    if (args?.length === 0) {
+      this._embed.setAuthor('Lion flipped a coin and it lands on...', this._image);
+      _responses = ['Heads', 'Tails']
+    } else {
+      this._image = 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/magic-wand_1fa84.png';
+      this._embed.setAuthor('Lion has chosen...', this._image);
+      _responses = args!;
+    }
+
     const choice = Math.floor(Math.random() * _responses.length)
     this._embed.setColor('#0099ff').setTitle(_responses[choice]);
-    this._embed.setAuthor('Lion flipped a coin and it lands on...', this._IMAGE);
+    
     message.reply(this._embed);
   }
 }
