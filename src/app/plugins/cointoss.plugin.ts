38cf5bba3ab524c0631d88baa4291c68acda0fe0
Elbert <etse168@gmail.com>
2021-06-03 16:54:30 -0400

    More changes

--- src/app/plugins/cointoss.plugin.ts
+++ src/app/plugins/cointoss.plugin.ts
@@ -8,27 +8,33 @@ export class CoinToss extends Plugin {
   public usage: string = '!cointoss || !pick <arg_1> <arg_2> ... <arg_n>';
   public pluginAlias: string[] = ['pick', 'select', 'cointoss', 'coin', 'toss', 'ct'];
   public permission: ChannelType = ChannelType.Public;
+  
   private _embed: MessageEmbed = new MessageEmbed();
   private _coinImg: string = 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/coin_1fa99.png';
+  private _wandImg: string = 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/magic-wand_1fa84.png';
   private _defaultArgs: string[] = ['Heads', 'Tails'];
-  private _responses: string[] = [];
+
   constructor(public container: IContainer) {
     super();
   }
 
-  public async execute(message: IMessage, args?: string[]) {
-    if (args?.length === 0) {
+  public async execute(message: IMessage, args: string[]) {
+    let responses: string[] = [];
+
+    if (args.length === 0) {
       this._embed.setAuthor('Lion flipped a coin and it lands on...', this._coinImg);
-      this._responses.push(...this._defaultArgs);
+      responses.push(...this._defaultArgs);
     } else {
-      const wandImg = 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/magic-wand_1fa84.png';
-      this._embed.setAuthor('Lion chooses...', wandImg);
-      this._responses.push(...args!);
+      this._embed.setAuthor('Lion chooses...', this._wandImg);
+      responses.push(...args);
     }
 
-    const choice = Math.floor(Math.random() * this._responses.length)
-    this._embed.setColor('#0099ff').setTitle(this._responses[choice]);
+    const choice = Math.floor(Math.random() * responses.length)
+    this._embed.setColor('#0099ff').setTitle(responses[choice]);
     
     message.reply(this._embed);
+
+    // Need to reset args list
+    responses = [];
   }
 }
