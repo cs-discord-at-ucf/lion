3569df62aa534cc3a73dbd1a6b4d2ed574107924
Tanner Sandlin <tanndlin@gmail.com>
2021-06-05 23:04:13 -0500

    Rewrote TA plugin for the DB and prof role (#447)
    * Rewrote TA plugin for the DB and prof role
    
    * DS
    
    * Check if already registered
    
    * DS
    
    * Returned strings to minimize number of replies

--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -2,6 +2,7 @@ import { MongoClient, Db, Collection } from 'mongodb';
 import { Moderation } from './moderation.service';
 import Environment from '../environment';
 import { LoggerService } from './logger.service';
+import { TAEntry as ITData } from '../app/plugins/ta.plugin';
 
 export class StorageService {
   private _db?: Db;
@@ -11,6 +12,7 @@ export class StorageService {
     modreports?: Collection<Moderation.IModerationReport>;
     modbans?: Collection<Moderation.IModerationBan>;
     modwarnings?: Collection<Moderation.IModerationWarning>;
+    classTAs?: Collection<ITData>;
   } = {};
 
   public constructor(private _loggerService: LoggerService) {
@@ -37,6 +39,7 @@ export class StorageService {
       this._collections.modreports = this._db.collection('modreports');
       this._collections.modbans = this._db.collection('modbans');
       this._collections.modwarnings = this._db.collection('modwarnings');
+      this._collections.classTAs = this._db.collection('classTAs');
 
       console.info(`Successfully connected to ${this._db.databaseName}`);
     } catch (e) {
