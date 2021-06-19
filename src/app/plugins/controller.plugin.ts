7c9ec49a8a344c922ebaec55f108edd01d8cd0f8
suneettipirneni <suneettipirneni@icloud.com>
2021-06-19 16:37:22 -0400

    Fix styling

--- src/app/plugins/controller.plugin.ts
+++ src/app/plugins/controller.plugin.ts
@@ -1,10 +1,10 @@
-import { Plugin } from "../../common/plugin";
-import { IContainer, ChannelType, IMessage } from "../../common/types";
+import { Plugin } from '../../common/plugin';
+import { IContainer, ChannelType, IMessage } from '../../common/types';
 
 export class PluginControl extends Plugin {
   public name: string = 'controller';
   public description: string = 'Controls activating and deactivating plugins.';
-  public usage: string = "!controller <activate | deactive> <plugin name>";
+  public usage: string = '!controller <activate | deactive> <plugin name>';
   public permission: ChannelType = ChannelType.Admin;
   public commandPattern: RegExp = /^(deactivate|activate) (?!\s*$).+/;
 
@@ -36,7 +36,7 @@ export class PluginControl extends Plugin {
     }
 
     try {
-      const result = await pluginStateData
+      await pluginStateData
         ?.updateOne({ name:  plugin.name }, 
           { $set: { isActive: method === 'activate' }},
           { upsert: true });
