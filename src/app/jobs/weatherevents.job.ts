0c4ce7bcb86b309f174c17365e688efc566a6e1f
Yousef Abdelsalam <yousefabdelsalam@knights.ucf.edu>
2021-06-27 03:08:12 -0400

    Updated interfaces and reversed warning orders

--- src/app/jobs/weatherevents.job.ts
+++ src/app/jobs/weatherevents.job.ts
@@ -6,6 +6,8 @@ import { TextChannel } from 'discord.js';
 export class WeatherEventsJob extends Job {
   // Run once an hour
   public interval: number = 1000 * 60 * 60;
+
+  // For debugging purposes only
   // public interval: number = 1000 * 10;
   public name: string = 'weather_events';
 
@@ -16,33 +18,36 @@ export class WeatherEventsJob extends Job {
   public async execute(container: IContainer) {
     const channel = (container.guildService.getChannel("weather_events")) as TextChannel;
     const resp = await container.httpService.get("https://api.weather.gov/alerts/active?area=FL");
-    const data: Weather_Event = resp.data;
+    const data: IWeather_Event = resp.data;
 
     if (!data.features || data.features.length == 0) {
       return;
     }
 
-    const relevantData: FeaturesEntity[] = data.features.filter(rD => {
+    const relevantData: IFeaturesEntity[] = data.features.filter(rD => {
       const highSeverity = rD.properties.severity === 'Severe' 
                           || rD.properties.severity === 'Extreme';
 
-      return highSeverity && rD.properties.headline.includes("Orlando")
+      return highSeverity && rD.properties.senderName.includes("Orlando")
     });
 
     const relevantHeadlines: string[] = relevantData.map(rD => rD.properties.headline)
 
-    channel.send(relevantHeadlines.join("\n")).catch(err => console.log(err));
+    // The warnings are automatically sorted from most to least recent, which
+    // is why I reversed it.
+    channel.send(relevantHeadlines.reverse().join("\n")).catch(err => console.log(err));
   }
 
 }
-export interface Weather_Event {
-  features?: (FeaturesEntity)[] | null;
+export interface IWeather_Event {
+  features?: (IFeaturesEntity)[] | null;
 }
 
-export interface FeaturesEntity {
-  properties: Properties;
+export interface IFeaturesEntity {
+  properties: IProperties;
 }
-export interface Properties {
+export interface IProperties {
   severity: string;
   headline: string;
+  senderName: string;
 }
