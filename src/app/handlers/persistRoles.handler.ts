053d33f0703f0f1a3a7087ad7ece55a3a48a1266
Tanndlin <tanndlin@gmail.com>
2021-06-28 12:16:15 -0500

    Moved Persisted Storage to User Service

--- src/app/handlers/persistRoles.handler.ts
+++ src/app/handlers/persistRoles.handler.ts
@@ -1,17 +1,11 @@
-import { GuildMember, Role, Snowflake } from 'discord.js';
-import { IContainer, IHandler, Maybe } from '../../common/types';
+import { GuildMember } from 'discord.js';
+import { IContainer, IHandler } from '../../common/types';
 
 export class PersistRolesHandler implements IHandler {
-  private _persistedRoles: Record<Snowflake, Role[]> = {};
-
   constructor(public container: IContainer) {}
 
   public execute(member: GuildMember) {
-    const roles = member.roles.cache;
-    this._persistedRoles[member.id] = Array.from(roles.values());
-  }
-
-  public getRoles(id: Snowflake): Maybe<Role[]> {
-    return this._persistedRoles[id];
+    const roles = Array.from(member.roles.cache.values());
+    this.container.userService.setPersistedRoles(member.id, roles);
   }
 }
