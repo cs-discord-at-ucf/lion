6fa59a0853e19b3c77337e8dc700044da018d100
Armintron <64503268+Armintron@users.noreply.github.com>
2021-06-28 14:19:21 -0400

    make members static and default export

--- src/app/plugins/reciperecommender.plugin.ts
+++ src/app/plugins/reciperecommender.plugin.ts
@@ -4,7 +4,7 @@ import Constants from '../../common/constants';
 import { Plugin } from '../../common/plugin';
 import { ChannelType, IContainer, IMessage } from '../../common/types';
 
-export class RecipeRecommenderPlugin extends Plugin {
+export default class RecipeRecommenderPlugin extends Plugin {
   public commandName: string = 'reciperecommender';
   public name: string = 'Recipe Recommender';
   public description: string =
@@ -14,11 +14,11 @@ export class RecipeRecommenderPlugin extends Plugin {
   public pluginAlias: string[] = ['recrec', 'rr', 'reciperecommender'];
   public pluginChannelName = Constants.Channels.Public.Food;
 
-  private readonly _API_URL = 'http://whatthefuckshouldimakefordinner.com/';
-  private readonly _RECIPE_NAME_REGEX = /\>(.*?)\</g;
-  private readonly _IMAGE_PREVIEW_REGEX = /<meta.*property="og:image".*content="(.*)".*\/>/;
-  private readonly _URL_REGEX = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&/=]*)/;
-  private readonly _AUTHOR_IMAGE_URL = 'https://i.imgur.com/B2vwvFL.png';
+  private static readonly _API_URL = 'http://whatthefuckshouldimakefordinner.com/';
+  private static readonly _RECIPE_NAME_REGEX = /\>(.*?)\</g;
+  private static readonly _IMAGE_PREVIEW_REGEX = /<meta.*property="og:image".*content="(.*)".*\/>/;
+  private static readonly _URL_REGEX = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&/=]*)/;
+  private static readonly _AUTHOR_IMAGE_URL = 'https://i.imgur.com/B2vwvFL.png';
 
   constructor(public container: IContainer) {
     super();
@@ -26,7 +26,7 @@ export class RecipeRecommenderPlugin extends Plugin {
 
   public async execute(message: IMessage, args: string[]) {
     const wantsVeg = args[0] === 'veg';
-    const API_URL = this._API_URL + (wantsVeg ? 'veg.php' : '');
+    const API_URL = RecipeRecommenderPlugin._API_URL + (wantsVeg ? 'veg.php' : '');
     const res = await axios.get(API_URL);
     if (!res || res.status !== 200) {
       await message.channel.send('Sorry, the API is down.');
@@ -36,15 +36,15 @@ export class RecipeRecommenderPlugin extends Plugin {
     // response page is always the same so we just take a specific line from the HTML response
     // based on wantsVeg, which changes the api endpoint used
     const scrapedRecipe: string = res.data.split('\n')[wantsVeg ? 23 : 21];
-    const [recipeURL] = scrapedRecipe.match(this._URL_REGEX)!;
+    const [recipeURL] = scrapedRecipe.match(RecipeRecommenderPlugin._URL_REGEX)!;
     // scrapes <meta content="og:image"> from recipie website
-    const [, previewImageURL] = (await axios.get(recipeURL)).data.match(this._IMAGE_PREVIEW_REGEX);
-    const [, uglyRecipeName] = scrapedRecipe.match(this._RECIPE_NAME_REGEX)!;
+    const [, previewImageURL] = (await axios.get(recipeURL)).data.match(RecipeRecommenderPlugin._IMAGE_PREVIEW_REGEX);
+    const [, uglyRecipeName] = scrapedRecipe.match(RecipeRecommenderPlugin._RECIPE_NAME_REGEX)!;
     const recipeName = uglyRecipeName.substring(1, uglyRecipeName.length - 1);
 
     const embed = new MessageEmbed();
     embed
-      .setAuthor('What the f*** should I make today?', this._AUTHOR_IMAGE_URL, API_URL)
+      .setAuthor('What the f*** should I make today?', RecipeRecommenderPlugin._AUTHOR_IMAGE_URL, API_URL)
       .setTitle(recipeName)
       .setURL(recipeURL)
       .setImage(previewImageURL ?? '');
