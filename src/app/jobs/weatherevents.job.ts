cc08c8c00a0c0445c2c9630864640c528a628b2c
Yousef Abdelsalam <yousefabdelsalam@knights.ucf.edu>
2021-06-28 22:20:49 -0400

    added await, moved and unexported the interfaces, and improved variable naming

--- src/app/jobs/weatherevents.job.ts
+++ src/app/jobs/weatherevents.job.ts
@@ -1,14 +1,25 @@
-// import { Mode } from '../../common/types';
 import { Job } from '../../common/job';
 import { IContainer } from '../../common/types';
 import { TextChannel } from 'discord.js';
+
+interface IWeather_Event {
+  features?: (IFeaturesEntity)[] | null;
+}
+
+interface IFeaturesEntity {
+  properties: IProperties;
+}
+
+interface IProperties {
+  severity: string;
+  headline: string;
+  senderName: string;
+}
 
 export class WeatherEventsJob extends Job {
   // Run once an hour
   public interval: number = 1000 * 60 * 60;
 
-  // For debugging purposes only
-  // public interval: number = 1000 * 10;
   public name: string = 'weather_events';
 
   constructor() {
@@ -24,30 +35,17 @@ export class WeatherEventsJob extends Job {
       return;
     }
 
-    const relevantData: IFeaturesEntity[] = data.features.filter(rD => {
-      const highSeverity = rD.properties.severity === 'Severe' 
-                          || rD.properties.severity === 'Extreme';
+    const headlines: string[] = data.features.filter(feature => {
+      const highSeverity = feature.properties.severity === 'Severe' 
+                          || feature.properties.severity === 'Extreme';
 
-      return highSeverity && rD.properties.senderName.includes("Orlando")
-    });
+      return highSeverity && feature.properties.senderName.includes("Orlando")
+    })
+    .map(feature => feature.properties.headline);
 
-    const relevantHeadlines: string[] = relevantData.map(rD => rD.properties.headline)
 
     // The warnings are automatically sorted from most to least recent, which
     // is why I reversed it.
-    channel.send(relevantHeadlines.reverse().join("\n")).catch(err => console.log(err));
+    await channel.send(headlines.reverse().join("\n")).catch(err => console.log(err));
   }
-
-}
-export interface IWeather_Event {
-  features?: (IFeaturesEntity)[] | null;
-}
-
-export interface IFeaturesEntity {
-  properties: IProperties;
-}
-export interface IProperties {
-  severity: string;
-  headline: string;
-  senderName: string;
 }
