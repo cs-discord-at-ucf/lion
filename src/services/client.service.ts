f3f395e8fea2b927ed87e796fcd859e443fa0135
Tanndlin <tanndlin@gmail.com>
2021-08-15 15:41:50 -0400

    Added more intents

--- src/services/client.service.ts
+++ src/services/client.service.ts
@@ -4,7 +4,14 @@ export class ClientService extends Client {
   private _startDate: Date;
 
   constructor() {
-    super({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES] });
+    super({
+      intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+      ],
+    });
     this.login(process.env.DISCORD_TOKEN);
     this._startDate = new Date();
   }
