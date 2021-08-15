5a2736d1f319a57ec58a437bbc58a693611c8ba7
Tanndlin <tanndlin@gmail.com>
2021-08-15 15:36:58 -0400

    presence

--- src/app/plugins/users.plugin.ts
+++ src/app/plugins/users.plugin.ts
@@ -1,6 +1,7 @@
 import { Plugin } from '../../common/plugin';
-import { IContainer, IMessage, ChannelType, IUser } from '../../common/types';
+import { IContainer, IMessage, ChannelType } from '../../common/types';
 import Constants from '../../common/constants';
+import { GuildMember } from 'discord.js';
 
 export default class UserCountPlugin extends Plugin {
   public commandName: string = 'users';
@@ -15,10 +16,10 @@ export default class UserCountPlugin extends Plugin {
   }
 
   public execute(message: IMessage) {
-    const members = [...this.container.clientService.users.cache.values()];
+    const members = [...this.container.guildService.get().members.cache.values()];
     const totalMembers = this.container.guildService.get().memberCount;
-    const onlineMembers = members.filter((member: IUser) => {
-      return member.presence.status !== 'offline';
+    const onlineMembers = members.filter((member: GuildMember) => {
+      return member.presence?.status !== 'offline';
     }).length;
     message.reply(
       `${Constants.ServerName} server currently has **${totalMembers} members** (${onlineMembers} currently online).`
