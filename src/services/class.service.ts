2c0141ac343ddf321724d0324fa2b63de059a9bb
Tanndlin <tanndlin@gmail.com>
2021-08-15 13:48:56 -0400

    Assert as GuildChannel

--- src/services/class.service.ts
+++ src/services/class.service.ts
@@ -193,6 +193,10 @@ export class ClassService {
 
   private _addClasses(): void {
     this._guild.channels.cache.forEach((channel) => {
+      if (channel.isThread()) {
+        return;
+      }
+
       if (!channel.parentID) {
         return;
       }
