a6f748a74b20f09c1cf1b95df87ed194000c6d00
Vijay Stroup <vijaystroup@gmail.com>
2021-08-21 20:44:57 -0400

    if not check first

--- src/app/handlers/moma.handler.ts
+++ src/app/handlers/moma.handler.ts
@@ -4,8 +4,10 @@ export class MomaHandler implements IHandler {
   constructor(public container: IContainer) {}
 
   public async execute(message: IMessage): Promise<void> {
-    if (message.content.toLowerCase().startsWith('who ')) {
-      await message.reply('https://tenor.com/view/nba-shaquille-o-neal-yo-moma-point-laugh-gif-4759702');
+    if (!message.content.toLowerCase().startsWith('who ')) {
+      return;
     }
+
+    await message.reply('https://tenor.com/view/nba-shaquille-o-neal-yo-moma-point-laugh-gif-4759702');
   }
 }
