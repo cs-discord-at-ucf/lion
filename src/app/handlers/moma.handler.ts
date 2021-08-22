c07574d05bba05ba8e21e8f3f5e3623f86da20a9
Vijay Stroup <vijaystroup@gmail.com>
2021-08-21 20:56:11 -0400

    limit to certain channels

--- src/app/handlers/moma.handler.ts
+++ src/app/handlers/moma.handler.ts
@@ -1,10 +1,26 @@
+import { TextChannel } from 'discord.js';
 import { IContainer, IHandler, IMessage } from '../../common/types';
+import Constants from '../../common/constants';
 
 export class MomaHandler implements IHandler {
+  private whitelistedChannels: Array<string> = [
+    Constants.Channels.Public.General,
+    Constants.Channels.Public.Memes,
+    Constants.Channels.Public.Vehicles,
+    Constants.Channels.Public.Fitness,
+    Constants.Channels.Public.Food,
+    Constants.Channels.Public.VideoGames
+  ]
+
   constructor(public container: IContainer) {}
 
   public async execute(message: IMessage): Promise<void> {
     if (!message.content.toLowerCase().startsWith('who ')) {
+      return;
+    }
+
+    const chan = message.channel as TextChannel;
+    if (!this.whitelistedChannels.includes(chan.name.toLowerCase())) {
       return;
     }
 
