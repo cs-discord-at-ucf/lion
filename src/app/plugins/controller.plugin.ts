ad51703749e0fbe9f1afc314fee7e6e53eeaf8e8
Tanner Sandlin <tanndlin@gmail.com>
2021-09-04 12:50:42 -0500

    Added List to Controller (#577)
    * Added List to Controller
    
    * DS

--- src/app/plugins/controller.plugin.ts
+++ src/app/plugins/controller.plugin.ts
@@ -1,6 +1,7 @@
+import { MessageEmbed } from 'discord.js';
 import Constants from '../../common/constants';
 import { Plugin } from '../../common/plugin';
-import { IContainer, ChannelType, IMessage, RoleType } from '../../common/types';
+import { IContainer, ChannelType, IMessage, RoleType, IPlugin } from '../../common/types';
 
 export default class PluginControl extends Plugin {
   public commandName: string = 'controller';
@@ -11,7 +12,7 @@ export default class PluginControl extends Plugin {
   public override pluginChannelName: string = Constants.Channels.Staff.ModCommands;
   public override minRoleToRun: RoleType = RoleType.Admin;
 
-  public override commandPattern: RegExp = /^(deactivate|activate) (?!\s*$).+/;
+  public override commandPattern: RegExp = /^(deactivate|activate|list)( (?!\s*$).)*/;
 
   constructor(public container: IContainer) {
     super();
@@ -20,11 +21,16 @@ export default class PluginControl extends Plugin {
   public async execute(message: IMessage, args: string[]): Promise<void> {
     const [method, pluginName] = args;
 
+    if (method.toLowerCase() === 'list') {
+      await this._listStatuses(message);
+      return;
+    }
+
     try {
       await this.container.pluginService.setPluginState(
         this.container,
         pluginName,
-        method === 'activate'
+        method.toLowerCase() === 'activate'
       );
     } catch (e) {
       await message.channel.send(e.message);
@@ -32,5 +38,30 @@ export default class PluginControl extends Plugin {
     }
 
     message.channel.send(`${pluginName} has been ${method}d`);
+  }
+
+  private _listStatuses(message: IMessage): Promise<IMessage> {
+    const plugins: IPlugin[] = Object.keys(this.container.pluginService.plugins).map((p) =>
+      this.container.pluginService.get(p)
+    );
+
+    const inactivePlugins = plugins.filter((p) => !p.isActive);
+
+    const embed = new MessageEmbed();
+    embed.setTitle('Plugin Statuses');
+    embed.setThumbnail(Constants.LionPFP);
+
+    embed.addField('Number of Plugins', plugins.length, true);
+    embed.addField('Number of inactive plugins', inactivePlugins.length);
+
+    if (inactivePlugins.length) {
+      embed.addField(
+        'Inactive Plugins',
+        inactivePlugins.map((p) => p.commandName).join('\n'),
+        true
+      );
+    }
+
+    return message.reply(embed);
   }
 }
