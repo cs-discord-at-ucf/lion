65418c22c26909b7ad90a025feeee97ad7a54284
Tanndlin <tanndlin@gmail.com>
2021-10-21 20:28:35 -0400

    New Intent for Guild Members

--- src/services/client.service.ts
+++ src/services/client.service.ts
@@ -7,6 +7,7 @@ export class ClientService extends Client {
     super({
       intents: [
         Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.DIRECT_MESSAGES,
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
