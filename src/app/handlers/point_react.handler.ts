a83be532052f72739fff6ac852e11a0c2e58c514
Tanndlin <tanndlin@gmail.com>
2022-01-04 10:40:11 -0500

    Disallow reacting to own message

--- src/app/handlers/point_react.handler.ts
+++ src/app/handlers/point_react.handler.ts
@@ -11,6 +11,20 @@ export class PointReactHandler extends Handler {
 
   public async execute(reaction: MessageReaction): Promise<void> {
     const message = reaction.message;
+    const userReacted = reaction.users.cache.last();
+    if (!userReacted) {
+      return;
+    }
+
+    // Make sure the person who reacted was not the bot or the person who posted the message
+    if (
+      [message.author?.id, this.container.clientService.user?.id].some(
+        (id) => id === userReacted.id
+      )
+    ) {
+      return;
+    }
+
     await this.container.pointService.awardPoints(message.author?.id, 1);
   }
 }
