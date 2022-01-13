40f186a21ab62d6f98d64182c37bbce1f6002ea2
Tanner Sandlin <tanndlin@gmail.com>
2022-01-13 18:10:27 -0500

    Give Points for Nitro Boosting (#637)

--- src/app/handlers/user_update.handler.ts
+++ src/app/handlers/user_update.handler.ts
@@ -5,14 +5,28 @@ import { IContainer } from '../../common/types';
 
 export class UserUpdateHandler extends Handler {
   public name: string = 'UserUpdate';
+  private _boostRewardAmount: number = 5000;
 
   constructor(public container: IContainer) {
     super();
   }
 
   public execute(oldUser: GuildMember, newUser: GuildMember): void {
-    // Don't ping mods
+    this._checkNameChange(oldUser, newUser);
+    this._checkBoost(oldUser, newUser);
+  }
 
+  private _checkBoost(oldUser: GuildMember, newUser: GuildMember) {
+    const boosterRole = this.container.guildService.getRole(Constants.Roles.NitroBooster);
+    if (oldUser.roles.cache.has(boosterRole.id) || !newUser.roles.cache.has(boosterRole.id)) {
+      return;
+    }
+
+    this.container.pointService.awardPoints(newUser.id, this._boostRewardAmount);
+  }
+
+  private _checkNameChange(oldUser: GuildMember, newUser: GuildMember) {
+    // Don't ping mods
     const shouldPing = !this.container.userService.hasRole(oldUser, Constants.Roles.Moderator);
     if (oldUser.displayName === newUser.displayName) {
       return;
