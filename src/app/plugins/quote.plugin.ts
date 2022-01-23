442431463ccfeb090bdeffca6c298cc522f33b4c
Tanner Sandlin <tanndlin@gmail.com>
2022-01-23 10:39:57 -0500

    Fix Quote Plugin (#649)
    * Revert "fix inconsistencies in quote plugin (#645)"
    
    This reverts commit 5c1cf7d7ccd7de5fe492eec90f4b9a6e497075d3.
    
    * Fixed Quote Plugin

--- src/app/plugins/quote.plugin.ts
+++ src/app/plugins/quote.plugin.ts
@@ -1,16 +1,11 @@
 import { Plugin } from '../../common/plugin';
-import { ChannelType, IContainer, IMessage } from '../../common/types';
+import { ChannelType, IContainer, IHttpResponse, IMessage, Maybe } from '../../common/types';
 import { MessageEmbed } from 'discord.js';
 
-export interface IAPIQuoteResponse {
-  _id: string;
+interface IQuote {
   tags: string[];
   content: string;
   author: string;
-  authorSlug: string;
-  length: number;
-  dateAdded: string;
-  dateModified: string;
 }
 
 export default class QuotePlugin extends Plugin {
@@ -18,10 +13,11 @@ export default class QuotePlugin extends Plugin {
   public name: string = 'Quote Plugin';
   public description: string = 'Prints a random quote';
   public usage: string = 'quote\nquote <tag>\nquote list';
+  public override pluginAlias = [];
   public permission: ChannelType = ChannelType.Public;
 
-  private static readonly _DEFAULT_URL: string = 'https://api.quotable.io/random' as const;
-  private static readonly _VALID_TAGS: readonly string[] = [
+  private _DEFAULT_URL: string = 'https://api.quotable.io/random';
+  private _VALID_TAGS: string[] = [
     'business',
     'education',
     'faith',
@@ -42,11 +38,7 @@ export default class QuotePlugin extends Plugin {
     'success',
     'technology',
     'wisdom',
-  ] as const;
-
-  private static readonly _VALID_TAGS_LIST_STR = `**Valid tags:** ${QuotePlugin._VALID_TAGS.join(
-    ', '
-  )}`;
+  ];
 
   constructor(public container: IContainer) {
     super();
@@ -61,10 +53,7 @@ export default class QuotePlugin extends Plugin {
       return true;
     }
 
-    const [tagInput] = args;
-    return (
-      tagInput.toLowerCase() === 'list' || QuotePlugin._VALID_TAGS.includes(tagInput.toLowerCase())
-    );
+    return args[0].toLowerCase() === 'list' || this._VALID_TAGS.includes(args[0].toLowerCase());
   }
 
   private _createEmbed(content: string, author: string, tag: string) {
@@ -72,7 +61,7 @@ export default class QuotePlugin extends Plugin {
       .setColor('#0099ff')
       .setTitle(tag === 'Famous-quotes' ? 'Famous-Quotes' : tag)
       .setFooter('Pulled from api.quotable.io')
-      .setTimestamp(Date.now())
+      .setTimestamp(new Date())
       .setDescription(`${content}\n\n- ${author}`);
   }
 
@@ -81,30 +70,37 @@ export default class QuotePlugin extends Plugin {
   }
 
   public async execute(message: IMessage, args: string[]) {
-    this.validate(message, args);
-    const [inputTag] = args;
-    const url: string =
-      args.length === 1
-        ? `${QuotePlugin._DEFAULT_URL}?tags=${inputTag.toLowerCase()}`
-        : QuotePlugin._DEFAULT_URL;
+    let url: string = this._DEFAULT_URL;
+    const [tagArg] = args;
 
-    if (inputTag.toLowerCase() === 'list') {
-      await message.reply(QuotePlugin._VALID_TAGS_LIST_STR);
-      return;
+    // Only if there was a tag given
+    if (tagArg) {
+      if (tagArg.toLowerCase() === 'list') {
+        await message.reply('**Valid tags:** ' + this._VALID_TAGS.join(', '));
+        return;
+      }
+
+      url = `${url}?tags=${tagArg.toLowerCase()}`;
     }
 
-    const response = await this.container.httpService
-      .get<IAPIQuoteResponse>(url)
+    await this._replyWithQuote(message, url, tagArg);
+  }
+
+  // A tag as null will generate the first tag associated with the random quote
+  private _replyWithQuote(message: IMessage, url: string, tag: Maybe<string>) {
+    return this.container.httpService
+      .get(url)
+      .then((response: IHttpResponse) => {
+        const quote = <IQuote>response.data;
+        const { content, author } = response.data;
+
+        const embed: MessageEmbed = this._createEmbed(
+          content,
+          author,
+          this._capitalizeTag(tag ?? quote.tags[0])
+        );
+        message.reply({ embeds: [embed] });
+      })
       .catch((err) => this.container.loggerService.warn(err));
-
-    if (!response) {
-      return;
-    }
-
-    const { content, author } = response.data;
-    const tag: string =
-      args.length > 0 ? this._capitalizeTag(inputTag) : this._capitalizeTag(response.data.tags[0]);
-
-    await message.reply({ embeds: [this._createEmbed(content, author, tag)] });
   }
 }
