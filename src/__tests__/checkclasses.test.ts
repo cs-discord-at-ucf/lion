1692c476af1e2015b34f3987596abcc56d52aad7
Tanndlin <tanndlin@gmail.com>
2022-03-15 19:41:54 -0400

    nit: spacing

--- src/__tests__/checkclasses.test.ts
+++ src/__tests__/checkclasses.test.ts
@@ -4,7 +4,6 @@ import { getContainerMock } from '../__mocks__';
 describe('CheckClasses Arg Tests', () => {
   test('Name with no spaces', () => {
     const plugin = new CheckClassesPlugin(getContainerMock());
-
     const input = 'Tanndlin#4450';
 
     const test = plugin.commandPattern.test(input);
@@ -13,7 +12,6 @@ describe('CheckClasses Arg Tests', () => {
 
   test('Name with spaces', () => {
     const plugin = new CheckClassesPlugin(getContainerMock());
-
     const input = 'Tanndlin Test#6270';
 
     const test = plugin.commandPattern.test(input);
@@ -22,7 +20,6 @@ describe('CheckClasses Arg Tests', () => {
 
   test('ID as handle', () => {
     const plugin = new CheckClassesPlugin(getContainerMock());
-
     const input = '97478270424985600';
 
     const test = plugin.commandPattern.test(input);
@@ -31,7 +28,6 @@ describe('CheckClasses Arg Tests', () => {
 
   test('ID with suffix should be false', () => {
     const plugin = new CheckClassesPlugin(getContainerMock());
-
     const input = '97478270424985600 hello';
 
     const test = plugin.commandPattern.test(input);
@@ -40,7 +36,6 @@ describe('CheckClasses Arg Tests', () => {
 
   test('ID with prefix should be false', () => {
     const plugin = new CheckClassesPlugin(getContainerMock());
-
     const input = 'hello 97478270424985600';
 
     const test = plugin.commandPattern.test(input);
