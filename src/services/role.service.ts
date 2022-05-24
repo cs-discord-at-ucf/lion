9732090936b8a2d7e2b3c69da316760228792b58
Maximo <63942620+maxfer1221@users.noreply.github.com>
2022-05-23 20:46:06 -0400

    Update role.service.ts

--- src/services/role.service.ts
+++ src/services/role.service.ts
@@ -1,23 +1,22 @@
-import { RoleType, RoleTypeKey } from './../common/types';
 import { GuildMember, Role, Collection } from 'discord.js';
 
+// Expected:
+// roles are a hierarchical enum/type. the following is the hierarchy (use these as the enum keys by convention):
+//   Admin
+//   Moderator
+//   Teaching Assistant
+//   RegularUser
+//   Suspended
+// an Admin can run everythin a Mod and below can run, a Mod can run everythin a TA and below can run ...
+// exception: 
+// Suspended role supersedes any other role. 
+// i.e., a member with both the with the Suspended and Admin roles can only run what a Suspended user can run
+//
+// by convention, define the enum/type in ./../common/types
 export class RoleService {
+  // Expected:
+  // returns a boolean marking if the member has permission to run the command
   hasPermission(member: GuildMember, minRoleToRun: number) {
-    const roles = member.roles;
-    const highestRole = this._getHighestRole(roles.cache);
-    return highestRole >= minRoleToRun;
-  }
-
-  private _getHighestRole(roles: Collection<string, Role>) {
-    let highestRole = 0;
-
-    roles.forEach((role: Role) => {
-      if (role.name in RoleType) {
-        if (role.name === 'Suspended') {return RoleType.Suspended;}
-        highestRole = Math.max(highestRole, RoleType[<RoleTypeKey>role.name]);
-      }
-    });
-
-    return highestRole;
+    // REWRITE
   }
 }
