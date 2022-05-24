eea2a7bca108388db3b46fa22eefde44204b9baa
Robert Boyd III <git@robertboyd.dev>
2022-05-23 20:07:26 -0700

    Reimplement RoleService
    This was implemented in a clean room, without reading the original
    implementation.

--- src/services/role.service.ts
+++ src/services/role.service.ts
@@ -1,7 +1,6 @@
+import { GuildMember } from 'discord.js';
 import { RoleType, RoleTypeKey } from './../common/types';
-import { GuildMember, Role, Collection } from 'discord.js';
 
-// Expected:
 // roles are a hierarchical enum/type. the following is the hierarchy (use these keys defind in src/common/types in 'RoleType'):
 //   Admin
 //   Moderator
@@ -9,13 +8,24 @@ import { GuildMember, Role, Collection } from 'discord.js';
 //   RegularUser
 //   Suspended
 // an Admin can run everythin a Mod and below can run, a Mod can run everythin a TA and below can run ...
-// exception: 
-// Suspended role supersedes any other role. 
+// exception:
+// Suspended role supersedes any other role.
 // i.e., a member with both the with the Suspended and Admin roles can only run what a Suspended user can run
 export class RoleService {
-  // Expected:
-  // returns a boolean marking if the member has permission to run the command
-  hasPermission(member: GuildMember, minRoleToRun: number) {
-    // REWRITE
+  hasPermission(member: GuildMember, minRoleToRun: RoleType): boolean {
+    let memberRole: RoleType = RoleType.RegularUser;
+    const orderedRoleTypes: RoleTypeKey[] = [
+      'Admin',
+      'Moderator',
+      'Suspended',
+      'Teaching Assistant',
+    ];
+    for (const roleType of orderedRoleTypes) {
+      if (member.roles.cache.find(role => role.name === roleType)) {
+        memberRole = RoleType[roleType];
+        break;
+      }
+    }
+    return memberRole >= minRoleToRun;
   }
 }
