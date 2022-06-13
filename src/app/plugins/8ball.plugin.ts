047e4c71f98eedf089f9ffb1acbfc28dad2d726c
Robert Boyd III <git@robertboyd.dev>
2022-06-12 22:26:00 -0700

    Reimplement 8ball
    This code was rewritten without any of the original source.
    
    I wasn't sure how safe copyright-wise it was to retain the original
    messages, so I wrote some new ones. Feel free to modify them to your
    liking in the future.

--- src/app/plugins/8ball.plugin.ts
+++ src/app/plugins/8ball.plugin.ts
@@ -1,19 +1,44 @@
-// REWRITE
-// Expected:
-// file should export a class named EightBallPlugin
-//
 // the old usage string was obviously '8ball', although it could be anything moving forward
 //
-// when executed, lion should respond with an embeded message containing 
-// one of the following strings (randomly picked, uniformly)
-//   bread gifts you with all the luck in the loaf. The best outcome is forseen.
-//   Corgo the Clever gifts you some of his infinite IQ. Outlook now favorable.
-//   Guaca the Goof teaches you the forbidden art of breaking the Golden Rule. Outlook favorable.
-//   The light of Szum shines upon you. Count on it.
-//   Guha the Great decides your fate. You are now an IT major.
-//   Joey reveals to you the sacred way of the Leetcoder. You may rely on it.
-//   Juan bullies bread, causing a chain reaction of unfavorable events. Probably not.
-//   Szum blesses you. It is certain.
-//   Szum the One True God punishes you for your blasphemy worshipping other false idols. Don't count on it.
-//   You have been cursed and Guaca the Goof is now your partner in a group project. Not looking so good...
-// and an embedded image of an 8 ball
+// when executed, lion should respond with an embed containing a message and an embedded image of an 8 ball
+
+import { MessageEmbed } from 'discord.js';
+import { Plugin } from '../../common/plugin';
+import { ChannelType, IContainer, IMessage } from '../../common/types';
+
+const replyOptions = [
+  'No.',
+  "It's not impossible, but I doubt it.",
+  "Don't bet on it.",
+  "It's about as likely as your code working on the first try.",
+  'Yes.',
+  'It is certain.',
+  'You can count on it.',
+  'I personally guarantee it.',
+  'Ask again later.',
+];
+
+const getRandom = <T>(array: T[]): T => array[Math.floor(Math.random() * array.length)];
+
+export default class EightBallPlugin extends Plugin {
+  public commandName = '8ball';
+  public name = 'Magic Eight Ball';
+  public description = 'Get answers from a pseudorandom number generator.';
+  public usage = '8ball';
+  public permission = ChannelType.All;
+
+  constructor(public container: IContainer) {
+    super();
+  }
+
+  public async execute(message: IMessage) {
+    await message.reply({
+      embeds: [
+        new MessageEmbed()
+          .setTitle(this.name)
+          .setThumbnail('https://publicdomainvectors.org/photos/Jarno_8_ball.png')
+          .setDescription(getRandom(replyOptions)),
+      ],
+    });
+  }
+}
