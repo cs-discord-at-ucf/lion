35dcf074a509d2201efdaf39a42694bc0e1c9231
Tanndlin <tanndlin@gmail.com>
2022-08-30 16:27:31 -0400

    Log error then throw

--- src/app/listener.ts
+++ src/app/listener.ts
@@ -31,11 +31,13 @@ export class Listener {
     this.container.clientService.on('ready', async () => {
       // Enforce only one server
       if (this.container.clientService.guilds.cache.size > 1) {
-        throw new Error(
+        this.container.loggerService.error(
           'More than one server detected. This bot is intented to run in only one server, ' +
             'and can cause issues when in multiple.\n Please remove this bot from other ' +
             'servers, or create a new application for Lion.'
         );
+
+        throw new Error('Too many guilds');
       }
 
       this.container.loggerService.info(`Loaded ${this.container.jobService.size()} jobs...`);
