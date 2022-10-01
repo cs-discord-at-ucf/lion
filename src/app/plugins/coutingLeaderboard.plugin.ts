9a872aaa9ac0e93d70f48ee64a9ae9940bfa7fad
Tanndlin <tanndlin@gmail.com>
2022-10-01 16:52:15 -0400

    Created plugin for showing leaderboard

---
+++ src/app/plugins/coutingLeaderboard.plugin.ts
@@ -0,0 +1,51 @@
+import { MessageEmbed } from 'discord.js';
+import Constants from '../../common/constants';
+import { Plugin } from '../../common/plugin';
+import { ChannelType, IContainer, ICountingEntry, IMessage } from '../../common/types';
+import { CountingLeaderboardModel } from '../../schemas/games.schema';
+
+export default class CountingLeaderboardPlugin extends Plugin {
+  public commandName = 'countingLeaderboard';
+  public name = 'Counting Leaderboard';
+  public description = 'Displays the top posters in #counting';
+  public usage = 'countlb';
+  public override pluginAlias = ['countlb'];
+  public permission = ChannelType.All;
+  public override pluginChannelName = Constants.Channels.Public.Games;
+
+  private readonly _DISPLAY_AMOUNT = 15;
+
+  constructor(public container: IContainer) {
+    super();
+  }
+
+  public async execute(message: IMessage) {
+    const docs = (await CountingLeaderboardModel.find({}))
+      .sort((a, b) => b.count - a.count)
+      .slice(0, this._DISPLAY_AMOUNT);
+
+    await message.reply({
+      embeds: [
+        new MessageEmbed()
+          .setTitle(this.name)
+          .addField(
+            'Leaderboard',
+            (
+              await Promise.all(
+                docs.map(
+                  async (userDoc: ICountingEntry, i: number) =>
+                    `${i + 1}. ${await this._convertICountingEntryToString(userDoc)}`
+                )
+              )
+            ).join('\n')
+          )
+          .setTimestamp(Date.now()),
+      ],
+    });
+  }
+
+  private _convertICountingEntryToString = async (userPoints: ICountingEntry) => {
+    const user = await this.container.clientService.users.fetch(userPoints.userId);
+    return `${user ?? userPoints.userId}: ${userPoints.count}`;
+  };
+}
