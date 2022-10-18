f55091b4c45cf288e2c1534b189baa619f6c7310
Pedro Fachetti Carvalho <pfachetti.pc@gmail.com>
2022-10-18 01:19:25 -0400

    Added grad_cafe channel to Constants object

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -32,6 +32,7 @@ const Constants = {
       StudyChat: 'study_chat',
       ClubOfficers: 'clubOfficers',
       AlumniLounge: 'alumni_lounge',
+      GradCafe: 'grad_cafe',
       GameknightChat: 'gameknight_chat',
       HorticultureChat: 'horticulture_chat',
       MovieknightChat: 'movieknight_chat',
