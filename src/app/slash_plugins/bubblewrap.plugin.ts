c17e4a86fde91c1cd2d6afb71251793b2087fcb2
Tanndlin <tanndlin@gmail.com>
2023-09-17 00:54:39 -0400

    feat: Convert bubblewrap to slash command

---
+++ src/app/slash_plugins/bubblewrap.plugin.ts
@@ -0,0 +1,14 @@
+import { Command } from '../../common/slash';
+
+const plugin = {
+  commandName: 'bubblewrap',
+  name: 'Bubble Wrap',
+  description: 'Sends the user a sheet of bubble wrap to pop',
+  async execute({ interaction }) {
+    const wrap: string =
+      'Here is some bubblewrap\n' + '||pop||||pop||||pop||||pop||||pop||\n'.repeat(5);
+    await interaction.reply(wrap);
+  },
+} satisfies Command;
+
+export default plugin;
